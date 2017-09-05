d9c0853a0633653d2b4d761b71712f795a454edf
kee nam <nam.keeyeon@gmail.com>
2017-09-05 16:43:47 -0700

    Added initialize tests for room_spec.rb

---
+++ specs/room_spec.rb
@@ -0,0 +1,21 @@
+require_relative 'spec_helper'
+
+describe "Room" do
+
+  before do
+    @room = Hotels::Room.new(5)
+  end
+
+  describe "#initialize" do
+    it "Can be initialized" do
+      @room.must_be_instance_of Hotels::Room
+    end
+    it "Has an ID number" do
+      @room.room_id.must_equal 5
+    end
+    it "Has a price per night that is an Integer" do
+      @room.price.must_be_kind_of Integer
+    end
+  end
+
+end
