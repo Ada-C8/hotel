bc70bdf5d2a583a34fc4b718d3db886d9d8853c5
Tanisha <tanishahampden@gmail.com>
2017-09-05 16:43:47 -0700

    fix file naming in spec_helper

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -8,9 +8,10 @@ require 'minitest/pride'
 require 'minitest/skip_dsl'
 require 'pry'
 
+
 # Require any classes
-require_relative '../lib/scoring'
-require_relative '../lib/player'
-require_relative '../lib/tilebag'
+require_relative '../lib/hotel'
+require_relative '../lib/reservation'
+require_relative '../lib/date_range'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
