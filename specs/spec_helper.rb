c90c20d44e26895baff86200dc8eb20592306f09
IsabelDePapel <isuchanek@gmail.com>
2017-09-05 13:28:51 -0700

    Set up project and test files

---
+++ specs/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+require_relative '../lib/room'
+require_relative '../lib/reservation'
+
+reporter_options = { color:true}
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(reporter_options)
+
+# Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
