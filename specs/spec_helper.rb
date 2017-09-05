bdc1eab1c40707a083de23579b2b740c52772e62
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-09-05 12:27:28 -0700

    add rakefile

---
+++ specs/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
