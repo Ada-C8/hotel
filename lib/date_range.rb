e4bdaebde615f6907edec0abf8281815f11eb464
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-09-05 16:36:02 -0700

    Added test for initialize method for class DateRange

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,5 +1,16 @@
 module BookingSystem
+
   class DateRange
+
+    def initialize(check_in, check_out)
+      @check_in = Date.strptime(check_in, '%d-%m-%Y')
+      @check_out = Date.strptime(check_out, '%d-%m-%Y')
+    end #end of initialize
+
+    def populate_dates
+
+    end #end of method
+
 
   end #end of class
 
