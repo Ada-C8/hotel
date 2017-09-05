27da86b2cb55104ca2a4646bc2e0ca387109fe86
kee nam <nam.keeyeon@gmail.com>
2017-09-05 15:58:35 -0700

    Added specs/spec_helper.rb file

---
+++ specs/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require 'minitest/pride'
+
+# Require any classes
+require_relative '../lib/hotel'
+require_relative '../lib/room'
+require_relative '../lib/reservation'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
