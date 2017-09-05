1f16195550c9155e4d0d19c2317d2cecef5a4685
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-09-05 16:24:05 -0700

    added room class

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,5 +1,23 @@
 module Hotel
+
   class Room
+    attr_reader :num
+
+    def initialize(num)
+      @num = num
+    end
+    
+    def self.all
+      rooms = []
+      (1..20).each do |num|
+        rooms << Room.new(num)
+      end
+      return rooms
+    end
 
   end
+
 end
+
+# thing = Hotel::Room.new(1).all
+# p thing
--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -1,1 +1,15 @@
 require_relative "spec_helper"
+
+describe "Room class" do
+  it "creates an instance of room" do
+    Hotel::Room.new(1).must_be_kind_of Hotel::Room
+  end
+  it "must respond to :num" do
+    Hotel::Room.new(1).must_respond_to :num
+  end
+end
+describe "Room.all" do
+  it "is an instance of Array" do
+    Hotel::Room.all.must_be_instance_of Array
+  end
+end
