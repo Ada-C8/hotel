387c25684f51f6aaf5d3d3b1d74690e226096a98
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-09-05 16:42:48 -0700

    add #all_rooms

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,14 +1,21 @@
 module Hotel
   class Hotel
-    attr_reader :rooms
-    # @reservations
+    attr_reader :rooms, :reservations
 
     def initialize(num_rooms)
       @rooms = []
       num_rooms.times do |i|
-        @rooms << Room.new(i)
+        @rooms << Room.new(i+1)
       end
+      @reservations = []
     end
+
+    def all_rooms
+      list = []
+      @rooms.each { |room| list << room.number }
+      list
+    end
+
     # #view_avail()
     # #view_booked()
     # #is_available?()
--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -1,16 +1,41 @@
 require_relative 'spec_helper'
 
-describe "Hotel" do
-  describe "Initialize" do
+
+describe 'Hotel' do
+  before do
+    @hotel = Hotel::Hotel.new(20)
+  end
+
+  describe '#initialize' do
     it "Can be instantiated" do
-      Hotel::Hotel.new(20).must_be_kind_of Hotel::Hotel
+      @hotel.must_be_kind_of Hotel::Hotel
     end
 
-    it "Creates an array of rooms" do
-      hotel = Hotel::Hotel.new(20)
+    it 'Creates an array of rooms' do
+      @hotel.rooms.must_be_kind_of Array
+      @hotel.rooms.length.must_equal 20
+    end
 
-      hotel.rooms.must_be_kind_of Array
-      hotel.rooms.length.must_equal 20
+    it 'Creates array of reservations' do
+      @hotel.reservations.must_equal []
     end
   end
+
+  describe '#all_rooms' do
+    # As an administrator, I can access the list of all of the rooms in the hotel
+    it 'Returns an array of all room numbers' do
+      @hotel.all_rooms.must_equal (1..20).to_a
+    end
+  end
+
+  describe '#make_reservation' do
+    @hotel.make_reservation('2017-09-05', '2017-09-08')
+
+    @hotel.reservations[0].must_be_kind_of Hotel::Reservation
+  end
+  # As an administrator, I can reserve a room for a given date range
+
+  # As an administrator, I can access the list of reservations for a specific date
+
+  # As an administrator, I can get the total cost for a given reservation
 end
