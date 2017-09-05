da9f53677eacbedf9c0f50e6e338a02a0120e1ee
Jessica Owens <vertige@gmail.com>
2017-09-05 13:45:51 -0700

    Added class, test and rake files for project setup

---
+++ specs/spec_helper.rb
@@ -0,0 +1,15 @@
+# SimpleCov needs to be FIRST THING in spec helper
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require 'minitest/pride'
+require_relative '../lib/room'
+require_relative '../lib/reservation'
+
+reporter_options = { color:true}
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(reporter_options)
+
+# Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
