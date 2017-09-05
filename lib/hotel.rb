750a0f6af73c6b6ea9750364e70fd7297787a434
Diane Lenertz <lenertzd@grinnell.edu>
2017-09-05 16:17:58 -0700

    create spec helper, rakefile, and lib/hotel and specs/hotel_spec

---
+++ lib/hotel.rb
@@ -0,0 +1,13 @@
+module Hotel
+  class Hotel
+  #
+  #   # def initialize
+  #   # end
+  #   #
+  #   # @reservations = []
+  #   #
+  #   # def view_available
+  #   # end
+  #
+  end #end of class
+end #end of module
---
+++ specs/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+require_relative '../lib/hotel'
+# require_relative '../lib/daterange'
+# require_relative '../lib/reservation'
