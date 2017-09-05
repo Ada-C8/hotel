86e2d67efbc59d50757d6842fef1d294453c73c5
Amy Lee <amy.yj.lee@gmail.com>
2017-09-05 14:44:30 -0700

    Created and tested initialize methods for Hotel::Room class

--- lib/Room.rb
+++ lib/Room.rb
@@ -1,8 +1,18 @@
+require 'pry'
 
 module Hotel
 
   class Room
+    attr_reader :id, :status, :nightly_rate
+
+    def initialize(id_number, nightly_rate = 200)
+      @id = id_number
+      @status =  :available
+      @nightly_rate =  nightly_rate
+    end
 
   end
 
 end
+
+# binding.pry
--- specs/Room_spec.rb
+++ specs/Room_spec.rb
@@ -1,1 +1,30 @@
 require_relative 'spec_helper'
+
+describe "Hotel::Room class" do
+  describe "initialize" do
+    it "requires one argument, number" do
+      proc {Hotel::Room.new}.must_raise ArgumentError
+
+      new_room = Hotel::Room.new(1)
+      new_room.must_be_instance_of Hotel::Room
+    end
+
+    it "creates a new Room object with the correct id, and initialized with the correct default values for status and nightly_rate" do
+      new_room = Hotel::Room.new(1)
+
+      new_room.id.must_equal 1
+      new_room.status.must_equal :available
+      new_room.nightly_rate.must_equal 200
+    end
+
+    it "can take an optional argument nightly_rate if we want to give it a different nightly_rate" do
+      vip_room = Hotel::Room.new(100, 350)
+
+      vip_room.id.must_equal 100
+      vip_room.nightly_rate.must_equal 350
+    end
+
+  end #end initialize
+
+
+end #end Hotel::Room class tests
