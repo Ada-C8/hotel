516eacb9772ad3ccf1115e57b28d3b7e82169cfb
Tanisha <tanishahampden@gmail.com>
2017-09-05 16:31:15 -0700

    project setup - rakefile, specs, and lib files

---
+++ specs/spec_helper.rb
@@ -0,0 +1,16 @@
+# specs/spec_helper.rb
+require 'simplecov'
+SimpleCov.start
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/pride'
+require 'minitest/skip_dsl'
+require 'pry'
+
+# Require any classes
+require_relative '../lib/scoring'
+require_relative '../lib/player'
+require_relative '../lib/tilebag'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
