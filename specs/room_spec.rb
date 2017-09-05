5cc1ce88dd360734d303f9d81d750732c73db3ab
murog <guillermina.muro@gmail.com>
2017-09-05 13:55:44 -0700

    green test create instance of room

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,1 +1,10 @@
 require_relative 'spec_helper'
+
+describe "room" do
+  describe "initialize" do
+    it "should be an instance of a room" do
+      input = Hotel::Room.new
+      input.must_be_instance_of Hotel::Room
+    end
+  end
+end
