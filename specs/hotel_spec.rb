af180a959cb6c305ea54720d6ee82a1dbf290d60
Rebecca Bergena <rbergena@gmail.com>
2017-09-05 16:57:10 -0700

    add tests for Hotel initialize method

--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -2,13 +2,28 @@ require_relative 'spec_helper'
 
 describe 'Hotel' do
 
-    describe "initialize" do
-      it "creates a new instance of hotel" do
-        Hotel.new.must_be_instance_of Hotel
-        # proc {Scrabble::Player.new(1)}.must_raise ArgumentError
-      end
+  describe "initialize" do
+    it "creates a new instance of hotel" do
+      Hotel.new.must_be_instance_of Hotel
     end
-      # it "holds a name" do
-      #   @player.name.must_equal "Ada"
-      # end
+
+    it "initialize with all rooms in hotel as array" do
+      Hotel.new.rooms.must_be_kind_of Array
+    end
+
+    it "room array elements range from 1 to 20" do
+      small_hotel = Hotel.new
+      small_hotel.rooms[0].must_equal 1
+      small_hotel.rooms[19].must_equal 20
+    end
+
+    it "can access list of all rooms in hotel" do
+      hotel_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+      Hotel.new.rooms.must_equal hotel_list
+
+    end
+  end
+  # it "holds a name" do
+  #   @player.name.must_equal "Ada"
+  # end
 end
