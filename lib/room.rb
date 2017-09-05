ec20b2542041bf4b3b8a9f00331b09ab8bfd6a97
Maria McGrew <mcgmar1@outlook.com>
2017-09-05 14:57:30 -0700

    creating a hash of rooms

--- lib/room.rb
+++ lib/room.rb
@@ -1,11 +1,13 @@
 module Hotel
   class Room
 
-    attr_accessor :room_num
+    attr_reader :room_num, :status, :dates_reserved
 
-    def initialize(room_num)
+
+    def initialize(room_num, status = :open, dates_reserved)
       @room_num = room_num
-
+      @status = status
+      @dates_reserved = dates_reserved
     end
 
   end #room
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -4,8 +4,16 @@ describe "Hotel#Room wave 1" do
   describe "instantiate room" do
 
     it "create a new room with a room number" do
-    room = Hotel::Room.new(4)
-    room.room_num.must_equal 4
+      room_num = 4
+      status = :open
+      dates_reserved = "look at dates"
+
+      room = Hotel::Room.new(room_num, status, dates_reserved)
+
+      room.must_respond_to :room_num
+      room.room_num.must_equal 4
+      room.room_num.must_be_kind_of Integer
+
   end
 
   end
