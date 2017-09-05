9256d0ded4100af315e3f7e833597c5756079dc8
Jessica Owens <vertige@gmail.com>
2017-09-05 14:09:16 -0700

    Created and tested Room initialization

---
+++ lib/room.rb
@@ -0,0 +1,20 @@
+module Hotel
+
+class Room
+#attr
+attr_reader :number
+
+#constants
+
+#initialize
+  def initialize(number)
+    @number = number
+  end
+#class methods
+
+#instance methods
+
+
+end # Room class
+
+end # Hotel module
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,1 +1,17 @@
 require_relative 'spec_helper'
+
+describe "Room" do
+  describe "initialize" do
+    it "Creates a room instance object with a room number" do
+      room = Hotel::Room.new(1)
+
+      room.must_be_instance_of Hotel::Room
+      room.number.must_equal 1
+    end
+
+    it "Creates a room with a rate" do
+
+    end
+
+  end
+end
