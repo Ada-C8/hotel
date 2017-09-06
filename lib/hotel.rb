84a27e8f41d08b320339b6fc2c4efd6edf23a4e3
Rebecca Bergena <rbergena@gmail.com>
2017-09-06 15:45:19 -0700

    add method to check room availability within date range and argument error in create reservation if a selected room is already booked

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -18,15 +18,19 @@ class Hotel
 
   # reserves an available room for a given date range
   def create_reservation(room, check_in, check_out)
-    # check availability of room for date range
-    # if
-    #   Reservation.new(room, check_in, check_out)
-    # else
-    # end
-    new_reservation = Reservation.new(room, check_in, check_out)
-    @@reservations << new_reservation
-    return new_reservation
 
+    if self.availability(check_in, check_out).include?(room)
+      # check availability of room for date range
+      # if
+      #   Reservation.new(room, check_in, check_out)
+      # else
+      # end
+      new_reservation = Reservation.new(room, check_in, check_out)
+      @@reservations << new_reservation
+      return new_reservation
+    else
+      raise ArgumentError.new("That room is not available")
+    end
   end
 
   def all_reservations
@@ -47,30 +51,58 @@ class Hotel
     return reservations_for_day
   end
 
-def availability
+  def availability(check_in, check_out)
+    reservations_for_range = []
+    reserved_rooms = []
+    date_range = DateRange.new(check_in, check_out)
+    day_to_check = date_range.start
+    date_range.nights.to_i.times do
+      reservations_for_range << [*check_date(day_to_check)]
+      day_to_check = day_to_check.next
+    end
+    reservations_for_range = *reservations_for_range
+    # print reservations_for_range
+    reservations_for_range.each do |reservation|
+      if !(reservation.empty?)
+        reserved_rooms << reservation[0].room
+        # handle if nil scenario return no reservations
+      end
+    end
+    available_rooms = @rooms - reserved_rooms.uniq
+    return available_rooms
+    # return *reservations_for_range
+  end
 
+  # def add_reservation(chec)
+  #
+  # end
+  # puts Date.new(2001,2,3)
 end
 
-# def add_reservation(chec)
-#
-# end
-# puts Date.new(2001,2,3)
-end
+bb = Hotel.new
+check_out = Date.new(2017, 03, 14)
+check_in = Date.new(2017, 03, 11)
+bb.create_reservation(1, check_in, check_out)
 
-# bb = Hotel.new
-# check_out = Date.new(2017, 03, 14)
-# check_in = Date.new(2017, 03, 11)
-# bb.create_reservation(1, check_in, check_out)
-#
-# check_in = Date.new(2017, 04, 8)
-# check_out = Date.new(2017, 04, 11)
-# bb.create_reservation(1, check_in, check_out)
-#
-# puts Hotel.all_reservations
-#
-# date = Date.new(2017, 03, 12)
-# puts Hotel.check_date(date)
+check_in = Date.new(2017, 04, 8)
+check_out = Date.new(2017, 04, 11)
+bb.create_reservation(2, check_in, check_out)
 
+puts bb.all_reservations
+
+date = Date.new(2017, 03, 12)
+puts bb.check_date(date)
+check_in = Date.new(2017, 04, 11)
+check_out = Date.new(2017, 04, 13)
+ bb.create_reservation(2, check_in, check_out)
+ puts bb.all_reservations
+
+#
+# check_in = Date.new(2017, 04, 9)
+# check_out = Date.new(2017, 04, 13)
+# print bb.availability(check_in, check_out)
+
+###################
 
 # puts Hotel.all_reservations[0]
 # test1 = Reservation.new(check_in, check_out)
