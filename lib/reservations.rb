ce5c68e9b1afe92e49f47eae6569b5e5316dedba
Severin Wiggenhorn <severin.randall@gmail.com>
2017-09-06 12:45:44 -0700

    valid_date_range method tests complete and passing

--- lib/reservations.rb
+++ lib/reservations.rb
@@ -9,13 +9,12 @@ module Hotel_System
   class Reservations
     attr_accessor :all_rooms, :check_in, :check_out, :res_room_number, :today
 
-    @today = Date.today
-    puts @today
 
     def initialize(room_num, check_in, check_out)
       @check_in = Date.parse(check_in)
       @check_out = Date.parse(check_out)
       @res_room_number = room_num
+      @today = Date.today
     end
 
     def avail?
@@ -23,17 +22,21 @@ module Hotel_System
     end
 
     def valid_date_range?
-      if @check_in < @today
+      if check_in < today
         raise ArgumentError.new("Invalid check in date")
-      elsif @check_out < @today
+      elsif check_out < today
         raise ArgumentError.new("Invalid check out date")
-      elsif @check_out < @check_in
+      elsif check_out < check_in
         raise ArgumentError.new("Invalid date range")
-      elsif @check_in == @check_out
+      elsif check_in == check_out
         raise ArgumentError.new("Check out must be after check in")
       else
         return true
       end
+    end
+
+    def num_of_nights
+      @num_of_nights = (@check_out - @check_in).to_i
     end
 
   end
--- specs/reservations_specs.rb
+++ specs/reservations_specs.rb
@@ -1,35 +1,71 @@
 require_relative 'spec_helper.rb'
+require 'date'
 
 describe "Hotel_System::Reservations" do
 
-it "can initialize a new reservation" do
-reservation = Hotel_System::Reservations.new(1, "1-1-2018", "1-5-2018")
-reservation.must_be_instance_of Hotel_System::Reservations
-end
+  it "can initialize a new reservation" do
+    reservation = Hotel_System::Reservations.new(1, "1-1-2018", "1-5-2018")
+    reservation.must_be_instance_of Hotel_System::Reservations
+  end
 
-it "accepts an appropriate check in date" do
-reservation = Hotel_System::Reservations.new(1, "1-1-2018", "1-5-2018")
-reservation.check_in.must_be_instance_of Date
-reservation.check_out.must_be_instance_of Date
-reservation.check_in.strftime("%Y-%m-%d").must_equal("2018-01-01")
-reservation.check_out.strftime("%Y-%m-%d").must_equal("2018-05-01")
-end
+  it "accepts an appropriate check in date" do
+    reservation = Hotel_System::Reservations.new(1, "1-1-2018", "1-5-2018")
+    reservation.check_in.must_be_instance_of Date
+    reservation.check_in.strftime("%Y-%m-%d").must_equal("2018-01-01")
+  end
 
-it "checks that the check in date is not in the past" do
+  it "accepts and appropriate check out date" do
+    reservation = Hotel_System::Reservations.new(1, "1-1-2018", "1-5-2018")
+    reservation.check_out.must_be_instance_of Date
+    reservation.check_out.strftime("%Y-%m-%d").must_equal("2018-05-01")
+  end
 
+  #TODO
+  it "valid date ranges will pass valid_date_range?" do
+    reservation = Hotel_System::Reservations.new(1, "1-1-2018", "1-5-2018")
+    # puts reservation.check_in < reservation.check_out
+    reservation.valid_date_range?.must_equal true
+  end
 
-end
+  it "checks that the check in date is not in the past" do
+    reservation = Hotel_System::Reservations.new(1, "1-1-2015", "1-1-2018")
+    proc {reservation.valid_date_range?}.must_raise ArgumentError
+    begin reservation.valid_date_range?
+    rescue ArgumentError => e
+      e.message.must_equal("Invalid check in date")
+    end
+  end
 
-it "checks the check out date is not in the past" do
+  it "checks the check out date is not in the past" do
+    reservation = Hotel_System::Reservations.new(1, "1-1-2018", "1-1-2015")
+    proc {reservation.valid_date_range?}.must_raise ArgumentError
+    begin reservation.valid_date_range?
+    rescue ArgumentError => e
+      e.message.must_equal("Invalid check out date")
+    end
+  end
 
-end
+  it "checks the check out date is after the check in date" do
+    reservation = Hotel_System::Reservations.new(1, "10-10-2017", "10-9-2017")
+    proc {reservation.valid_date_range?}.must_raise ArgumentError
+    begin reservation.valid_date_range?
+    rescue ArgumentError => e
+      e.message.must_equal("Invalid date range")
+    end
+  end
 
-it "checks the check out date is after the check in date" do
+  it "checks the check in date is not the same as the check out date" do
+    reservation = Hotel_System::Reservations.new(1, "10-10-2017", "10-10-2017")
+    proc {reservation.valid_date_range?}.must_raise ArgumentError
+    begin reservation.valid_date_range?
+    rescue ArgumentError => e
+      e.message.must_equal("Check out must be after check in")
+    end
+  end
 
-end
-
-it "checks the check in date is not the same as the check out date" do
-
-end
+  it "correctly calculates the number of nights of the stay" do
+    reservation = Hotel_System::Reservations.new(1, "2018-1-1", "2018-1-5")
+    reservation.num_of_nights.must_equal 4
+  end
 
 end
