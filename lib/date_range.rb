12e4bc2411050bb8343d0e8ab64a306ea9d452c3
Andrea Chen <andreachen.ac@gmail.com>
2017-09-06 10:00:16 -0700

    Create DateRange class with constructor. Create corresponding tests, all passing.

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,1 +1,13 @@
 require 'date'
+
+module BookingSystem
+  class DateRange
+    attr_reader :check_in, :check_out
+
+    def initialize(check_in, check_out)
+      @check_in = check_in
+      @check_out = check_out
+    end
+
+  end
+end
--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -1,1 +1,28 @@
 require_relative 'spec_helper'
+require 'date'
+
+describe "DateRange class" do
+
+  before do
+    check_in = Date.new(2017,9,9)
+    check_out = Date.new(2017,9,11)
+    @test_ob = BookingSystem::DateRange.new(check_in, check_out)
+  end
+
+  describe "intialize method" do
+
+    it "Can be initialized" do
+      @test_ob.must_be_instance_of BookingSystem::DateRange
+    end
+
+    it "Has an instance variable @check_in that is an instance of Date" do
+      @test_ob.check_in.must_be_instance_of Date
+    end
+
+    it "Has an instance variable @check_out that is an instance of Date" do
+      @test_ob.check_out.must_be_instance_of Date
+    end
+  end
+
+
+end
