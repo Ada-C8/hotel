fff6b7709ee22b5358ec1d16f3824b5f70862a74
Diane Lenertz <lenertzd@grinnell.edu>
2017-09-06 12:00:06 -0700

    add include? and overlap? methods and tests for DateRange

--- lib/daterange.rb
+++ lib/daterange.rb
@@ -1,45 +1,63 @@
 require 'date'
 
 module Hotel
-class DateRange
-  attr_reader :date1, :date2
+  class DateRange
+    attr_reader :date1, :date2
 
-  def initialize(date1, date2)
-    @date1 = date1
-    @date2 = date2
-  end
-
-  def one_day?
-    if date1 == date2
-      return true
-    else
-      return false
+    def initialize(date1, date2)
+      @date1 = date1
+      @date2 = date2
+      # @one_day = true
+      one_day?
+      #puts "one_day?: #{one_day?} class: #{one_day?.class}"
+      beginning
+      #puts "beginning: #{beginning} class: #{beginning.class}"
+      ending
+      #puts "ending: #{ending} class #{ending.class}"
     end
-  end
 
-  def begin
-    if !one_day? && date1 < date2
-        @beginning = date1
-      elsif !one_day? && date2 < date1
-        @beginning = date2
+    def one_day?
+      if @date1 == @date2
+        return true
+      else
+        return false
       end
+    end
+
+    def beginning
+      if !one_day? && @date1 < @date2
+        @beginning = @date1
+      elsif !one_day? && (@date2 < @date1)
+        @beginning = @date2
+      end
+    end
+
+    def ending
+      if !one_day? && @date1 > @date2
+        @ending = @date1
+      elsif !one_day? && @date2 > @date1
+        @ending = @date2
+      end
+    end
+
+
+    def include?(date)
+      if (one_day? && date == @date1) || (@beginning <= date && date <= @ending)
+        return true
+      else
+        return false
+      end
+    end
+
+    def overlap?(daterange)
+      if include?(daterange.beginning) || include?(daterange.ending)
+        return true
+      elsif daterange.include?(@beginning) || daterange.include?(@ending)
+        return true
+      else
+        return false
+      end
+    end
+
   end
-
-  #def end
-    #if !one_day? && date1 is after date2
-      #@end = date1
-    #elsif !one_day? && date2 is after date1
-      #@end = date2
-  #  end
-    #end
-
-
-  #
-  # def include?
-  # end
-  #
-  # def overlap?
-  # end
-
-end
 end
--- specs/daterange_spec.rb
+++ specs/daterange_spec.rb
@@ -34,7 +34,7 @@ describe 'DateRange' do
 
   end
 
-  describe 'begin' do
+  describe 'beginning' do
     before do
       @date1 = Date.new(2017,9,12)
       @date2 = Date.new(2017,9,18)
@@ -42,35 +42,85 @@ describe 'DateRange' do
       @daterange2 = Hotel::DateRange.new(@date2, @date1)
     end
     it 'must be a kind of date' do
-      @daterange.begin.must_be_kind_of Date
+      @daterange.beginning.must_be_kind_of Date
     end
-    it 'must identify the correct beginning date' do
-      @daterange.begin.must_equal @date1
+    it 'must identify the correct beginningning date' do
+      @daterange.beginning.must_equal @date1
     end
-    it 'must identify the correct beginning date even if passed second' do
-      @daterange2.begin.must_equal @date1
+    it 'must identify the correct beginningning date even if passed second' do
+      @daterange2.beginning.must_equal @date1
     end
   end
-  #
-  # describe 'end' do
 
-  #   daterange.end.must_be_kind_of Date
-  #   @daterange.end(@date1, @date2).must_equal @date2
-  # end
+  describe 'ending' do
+    before do
+      @date1 = Date.new(2017,9,12)
+      @date2 = Date.new(2017,9,18)
+      @daterange = Hotel::DateRange.new(@date1, @date2)
+      @daterange2 = Hotel::DateRange.new(@date2, @date1)
+    end
+    it 'must be kind of date' do
+      @daterange.ending.must_be_kind_of Date
+    end
+    it 'must identify the correct end date if passed second' do
+      @daterange.ending.must_equal @date2
+    end
+    it 'must identify the correct end date if passed first' do
+      @daterange2.ending.must_equal @date2
+    end
+  end
 
-  # describe 'include?' do
-  #   it 'returns a boolean' do
-  #     included_date = Date.new(2017,9,13)
-  #     excluded_date = Date.new(2017,10,12)
-  #     @daterange.include?(included_date).must_equal true
-  #     @daterange.include?(excluded_date).must_equal false
-  #   end
-  # end
+  describe 'include?' do
+    before do
+      @date1 = Date.new(2017,9,12)
+      @date2 = Date.new(2017,9,18)
+      @date3 = Date.new(2017,9,15)
+      @date4 = Date.new(2017,9,20)
+      @daterange = Hotel::DateRange.new(@date1, @date2)
+      @daterange2 = Hotel::DateRange.new(@date2, @date1)
+    end
+
+    it 'returns a true if the date is included' do
+      @daterange.include?(@date3).must_equal true
+      @daterange2.include?(@date3).must_equal true
+    end
+
+    it 'returns false if the date is not included' do
+      @daterange.include?(@date4).must_equal false
+      @daterange2.include?(@date4).must_equal false
+    end
+
+    it "returns true if the date range is just one day" do
+      daterange = Hotel::DateRange.new(@date1, @date1)
+      daterange.include?(@date1).must_equal true
+    end
+  end
 
   describe 'overlap' do
+    before do
+      @date1 = Date.new(2017,9,12)
+      @date2 = Date.new(2017,9,18)
+      @date3 = Date.new(2017,9,15)
+      @date4 = Date.new(2017,9,20)
+      @date5 = Date.new(2017,9,17)
+      @date6 = Date.new(2017,9,11)
+      @date7 = Date.new(2017,9,2)
+      @daterange = Hotel::DateRange.new(@date1, @date2)
+      @daterange2 = Hotel::DateRange.new(@date3, @date4)
+      @daterange3 = Hotel::DateRange.new(@date3, @date5)
+      @daterange4 = Hotel::DateRange.new(@date3, @date6)
+      @daterange5 = Hotel::DateRange.new(@date4, @date6)
+      @daterange6 = Hotel::DateRange.new(@date6, @date7)
+    end
+    it 'returns true if the date range overlaps with the passed date range by one or all dates' do
+      @daterange.overlap?(@daterange2).must_equal true
+      @daterange.overlap?(@daterange3).must_equal true
+      @daterange.overlap?(@daterange4).must_equal true
+      @daterange.overlap?(@daterange5).must_equal true
+    end
+    it 'returns false if the date range does not overlap with the passed date range' do
+      @daterange.overlap?(@daterange6).must_equal false
+    end
 
   end
-
-
-
 end
