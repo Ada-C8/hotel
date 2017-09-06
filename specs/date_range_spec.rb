329fe9bb32ba50e92287f48144d8f39b5e88630b
nkiru onwuneme <generalhrh@gmail.com>
2017-09-06 14:21:47 -0700

    Refactored tests

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -3,49 +3,35 @@
 require_relative 'spec_helper'
 
 describe "Date Range" do
+
+  before do
+    checkin_date = Date.new(2001,2,3)
+    checkout_date = Date.new(2001,2,14)
+    @date = Hotel::DateRange.new(checkin_date, checkout_date)
+  end
+
   describe "initialize method" do
     it "takes checkin and checkout dates" do
-      checkin_date = Date.new(2001,2,3)
-      checkout_date = Date.new(2001,2,14)
-      date =  Hotel::DateRange.new(checkin_date, checkout_date)
-      date.must_be_instance_of Hotel::DateRange
+      @date.must_be_instance_of Hotel::DateRange
     end
   end
 
   describe "date include" do
     it "checks if a date is included in the date range" do
-      checkin_date = Date.new(2001,2,3)
 
-      checkout_date = Date.new(2001,2,14)
-
-      date =  Hotel::DateRange.new(checkin_date, checkout_date)
-
-      input_date = Date.new(2001,2,5)
-
-      date.date_include?(input_date) .must_equal true
+      @date.date_include?(Date.new(2001,2,5)) .must_equal true
     end
 
     it "raises an error for an invalid date" do
-      checkin_date = Date.new(2001,2,3)
-
-      checkout_date = Date.new(2001,2,14)
-
-      date =  Hotel::DateRange.new(checkin_date, checkout_date)
-
-      proc{date.date_include?(Date.new(2007,15,5))}.must_raise ArgumentError
-
+      proc{@date.date_include?(Date.new(2007,15,5))}.must_raise ArgumentError
     end
   end
 
-
-  # describe "date overlap" do
-  #   it "check if the "
-  # end
-
+  describe "date overlap" do
+    it "it checks if the dates are within the checkin/checkout date range" do
+  end
 
 
-
-    #
     # it "checks if only the checkin date is included in the date range" do
     #
     # end
