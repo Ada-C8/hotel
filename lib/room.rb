0ad43860c567642757bd98f43d1cb1a7eb59f492
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-06 03:23:59 -0700

    Created initial tests for Room class and created self.all method for Room

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,8 @@
-module Hotel
-  class Room
+require 'date'
 
+module Hotel
+  NUM_OF_ROOMS = 20
+  class Room
     attr_reader :available, :check_in_date, :check_out_date, :cost
 
     def initialize
@@ -9,6 +11,19 @@ module Hotel
       @check_in_date = nil
       @check_out_date = nil
       @cost = 200
+    end
+
+    def self.all
+      room_hash = {}
+      NUM_OF_ROOMS.times do |i|
+        current_room_hash = {i+1 => Hotel::Room.new} # Hotel Room Numbers start with 1, not 0
+        room_hash.merge!(current_room_hash)
+      end
+      return room_hash
+    end
+
+    def change_availability
+      @available ? @available = false : @available = true
     end
 
   end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -5,10 +5,31 @@ describe "Room" do
     @room = Hotel::Room.new()
   end
 
-  describe "Cost" do
+  describe "initialize" do
+    it "Has an available attribute that is initialized to true" do
+      @room.available.must_equal true
+    end
+
+    it "Has a check_in_date and check_out_date initialized to nil (because it's unreserved yet)" do
+    # initializing to nil because a simple Date.new would give you a -4721 BC date,
+    # it doesn't make sense to initialize a mock value
+      assert_nil(@room.check_in_date)
+      assert_nil(@room.check_out_date)
+    end
+
     it "(Wave 1) Returns the cost of the room." do
       @room.cost.must_be_instance_of Integer
       @room.cost.must_equal 200
+    end
+  end
+
+  describe "self.all" do
+    it "Will create a hash composed of the 20 rooms in the hotel" do
+      all_rooms = Hotel::Room.all
+      all_rooms.must_be_instance_of Hash
+      all_rooms.length.must_equal 20
+      all_rooms[rand(20) + 1].must_be_instance_of Hotel::Room
+      all_rooms.keys[rand(20)].must_be_instance_of Integer
     end
   end
 
