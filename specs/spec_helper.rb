3115b7c457751c0a87a25ec895225d07c25392ae
nkiru onwuneme <generalhrh@gmail.com>
2017-09-06 13:38:01 -0700

    Added date_range file

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -2,11 +2,15 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'minitest/reporters'
+require 'date'
 
 
 require 'simplecov'
 SimpleCov.start
 
 #require any classes
+require_relative '../lib/date_range'
+
+
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
