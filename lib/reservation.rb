a487c11db57d8ed56c98b634f829f71b68c3a330
Eva Sawyer <esawyer25@gmail.com>
2017-09-05 21:57:47 -0700

    set up initialize

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,14 +1,20 @@
+require_relative 'date_range'
 module My_Hotel
   class Reservation
 
-    attr_reader :reservation_id, :date_range, :block_id, :rate, :renter_name
+    attr_reader :reservation_id, :block_id, :date_range
 
-    def initialize(start_date, end_date, block_id = 0, renter_name = "Casper")
-      @reservation_id = set_id
-      @date_range = Date_Range.date_range(start_date, end_date)
-      @block_id = block_id
-      @renter_name = renter_name #could be expanded to include more info
+    def initialize(start_date,end_date)
+      @start_date = start_date
+      @end_date = end_date
+      @reservation_id = 1
+      @date_range = My_Hotel::Date_Range.new(@start_date, @end_date).date_range
+      @block_id = 1
     end
+
+    # def get_date_range
+    #   My_Hotel::Date_Range.new(@start_date, @end_date).date_range
+    # end
 
     # def set_id
     #   make a random id number for the reservation
@@ -22,8 +28,9 @@ module My_Hotel
     # def cancel_reservation
     # end
 
-    # def self.all #returns a list of all the reservations
-    # end
+    #  def self.all
+    #    puts
+    #  end
 
   end
 end
