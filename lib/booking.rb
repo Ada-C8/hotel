9891c1354017e7fcd16e1d8d55b39de887388f0b
Tamira Vojnar <tamiravojnar@gmail.com>
2017-09-06 14:27:58 -0700

    Worked on make_reservation method so that it makes an instance of Reservation. Finishing tests for what is in Reservation instance

--- lib/booking.rb
+++ lib/booking.rb
@@ -5,7 +5,7 @@
     # It will have an instance variable @all_reservations which will be an array of all the Reservation instances created in the Booking class
     # It will have a method that makes a new reservation
         # Before it creates a new Reservation it will need to verify that the hotel has vacancy for the date(s) in question
-        #Once it verifies that there is availiblility it will create a new instance of Reservation with a res_id (unique to each Reservation @all_reservations.length + 1? ), total_cost (# of rooms * number of nights * cost of a room), res_rooms (the instsances of the Room class that are included in the Reservation, identified by Room.room_number), date_range (get from the user as a starting and ending date and pass to DateRange to get the date_range argument)
+        #Once it verifies that there is availiblility it will create a new instance of Reservation with a reservation_id (unique to each Reservation @all_reservations.length + 1? ), total_cost (# of rooms * number of nights * cost of a room), res_rooms (the instsances of the Room class that are included in the Reservation, identified by Room.room_number), date_range (get from the user as a starting and ending date and pass to DateRange to get the date_range argument)
               # using the availible array we can iterate though the array to create new Reservation for the date(s) requested with the right rooms (only the ones in the availible array)
     # it will have a method that checks if there is a reservation for the date(s) requested
         # iterate though @all_reservations for each day requested and if the day is included in the Booking then it will look at the Reservation that included that date and add the room(s) in that reservation to a new array not_availible.
@@ -29,8 +29,21 @@ module Hotel
       end
     end # initialize
 
-    def make_reservation
-      @all_reservations << Hotel::Reservation.new 
-    end
+    def make_reservation(start_date, end_date, num_rooms )
+      dates_booked = Hotel::DateRange.new(start_date, end_date).nights_booked
+      reservation_id = @all_reservations.length + 1
+      rooms = []
+      i = 0
+      num_rooms.times do
+        rooms << @all_rooms[i]
+        i += 1
+      end # TODO: later I need to change this to pull from the availible rooms array
+      cost = (num_rooms * dates_booked.length * 20.0)
+
+      @all_reservations << Hotel::Reservation.new(reservation_id, cost, rooms, dates_booked)
+    end # make_reservation
+
+    # def check_date_for_reservations
+    # end # check_date_for_reservations
   end # Booking
 end # Hotel
--- specs/booking_spec.rb
+++ specs/booking_spec.rb
@@ -4,6 +4,7 @@ require 'date'
 describe "Booking" do
   before do
     @booking = Hotel::Booking.new
+
   end # before
   describe "initialize" do
 
@@ -37,28 +38,73 @@ describe "Booking" do
   end # describe initialize
 
   describe "make_reservation" do
-    @new_reservation = Hotel::Booking.make_reservation
+    # before do
+    #   @booking.make_reservation()
+    # end # before
 
-    it "will create a new instance of Reservation" do
-      @new_reservation.must_be_kind_of Hotel::Reservation
+    it "should be able to be called on @booking" do
+      @booking.must_respond_to :make_reservation
+    end #it "should be able to be called on @booking" do
+
+    it "should create the correct value for dates_booked" do
+      checkin_day = Date.new(2017, 9, 5)
+      checkout_day = Date.new(2017, 9, 7)
+      num_of_rooms = 1
+      @booking.make_reservation(checkin_day, checkout_day, num_of_rooms )[0].date_range.must_equal [checkin_day, (checkin_day + 1)]
+    end # it "should create the correct value for date_booked" do
+
+    it "will create the right reservation id for a booking" do
+      checkin_day = Date.new(2017, 9, 5)
+      checkout_day = Date.new(2017, 9, 7)
+      num_of_rooms = 1
+      @booking.make_reservation(checkin_day, checkout_day, num_of_rooms )[0].res_id.must_equal 1
+    end # it "will create the right reservation_id for a booking" do
+
+    it "will have the right number of rooms in the reservation when asked to book 1 room" do
+      checkin_day = Date.new(2017, 9, 5)
+      checkout_day = Date.new(2017, 9, 7)
+      num_of_rooms = 1
+      @booking.make_reservation(checkin_day, checkout_day, num_of_rooms )[0].res_rooms.length.must_equal 1
+    end # it "will have the right number of rooms in the reservation when asked to book 1 room" do
+
+    it "will have the right number of rooms in the reservation if three rooms are asked for" do
+      checkin_day = Date.new(2017, 9, 5)
+      checkout_day = Date.new(2017, 9, 7)
+      num_of_rooms = 3
+      @booking.make_reservation(checkin_day, checkout_day, num_of_rooms )[0].res_rooms.length.must_equal 3 
+    end # it "will have the right number of rooms in the reservation if three rooms are asked for" do
+
+
+
+
+
+
+
+
+
+
+
+    xit "will create a new instance of Reservation" do
+      @booking.all_reservations[0].must_be_kind_of Hotel::Reservation
     end # it "will create a new instance of Reservation" do
 
-    it "will create the right reservation id for each reservation" do
+    xit "will create the right reservation id for each reservation" do
       # TODO
     end # it "will create the right reservation id for each reservation" do
 
-    it "will create a reservation for one room" do
+    xit "will create a reservation for one room" do
       # TODO
     end # it "will create a reservation for one room" do
 
-    it "will create a reservation for two rooms" do
+    xit "will create a reservation for two rooms" do
       # TODO
     end # it "will create a reservation for two rooms" do
 
-    it "will have the correct cost of the reservation" do
+    xit "will have the correct cost of the reservation" do
       # TODO
     end # it "will have the correct cost of the reservation" do
 
   end #  describe make_reservation
+
 
 end # Booking
