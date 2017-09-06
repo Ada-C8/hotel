3499b1367e60e862ed39c3f52a5210c100422c15
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-06 14:21:13 -0700

    Adds attr reader helper method to booking class

--- lib/booking.rb
+++ lib/booking.rb
@@ -3,12 +3,18 @@ require 'date'
 
 module Hotel
   class Booking
-    #initialize will have the following parameters (date_range, num_rooms)
-    #id
-    #total cost
-    #room(s)
-    #date_range
-    def total_cost
+    attr_reader :id, :rooms, :date_range, :nights, :total_cost
+
+    def initialize(id, rooms, date_range)
+      @id = id
+      @rooms = rooms
+      @date_range = date_range
+      @nights = date_range.nights
+      @total_cost = ''
     end
+    #initialize will have the following parameters
+    #@rooms = @rooms.each {|room| @total_cost += room.cost}
+    # def total_cost
+    # end
   end
 end
