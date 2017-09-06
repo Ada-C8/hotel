a9371e0bd5f0de7d47684df9af5dac68b2c851c7
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-09-05 20:42:49 -0700

    Added tests for #initialize method and passed all of them

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,15 +1,31 @@
+require 'date'
+
 module BookingSystem
 
   class DateRange
 
+    attr_reader :check_in, :check_out
+
     def initialize(check_in, check_out)
-      @check_in = Date.strptime(check_in, '%d-%m-%Y')
-      @check_out = Date.strptime(check_out, '%d-%m-%Y')
+      @check_in = check_in
+      @check_out = check_out
+
+      if check_in.class != Date || check_out.class != Date || @check_in > @check_out
+        raise ArgumentError.new("Invalid date range")
+      end
     end #end of initialize
 
-    def populate_dates
+
+    def dates_within_range
 
     end #end of method
+
+    # def valid_dates?
+    #   if @check_in < @check_out
+    #     return true
+    #   end
+    #   raise ArgumentError.new("Invalid date range")
+    # end #end of method
 
 
   end #end of class
--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -1,19 +1,43 @@
-require_relative 'date_range'
+require_relative 'spec_helper'
 
-describe "#initialize" do
-  it "Check_in value can be called by .check_in" do
-    new_date_range = BookingSystem::DateRange.new("09-15-2017", "09-17-2017")
-    new_date_range.check_in.must_equal "2017-09-15"
-    new_date_range.check_out.must_equal "2017-09-17"
+describe "DateRange" do
+  before do
+    @check_in = Date.new(2017,9,15)
+    @check_out = Date.new(2017,9,17)
+    @new_date_range = BookingSystem::DateRange.new(@check_in, @check_out)
   end
 
-  xdescribe "" do
-    it "Returns true if given date range is valid" do
-      date_range = BookingSystem::DateRange.new("09-15-2017", "09-17-2017")
-
+  describe "#initialize" do
+    it "Check_in and check_out values are types of Date class" do
+      @new_date_range.check_in.must_be_kind_of Date
+      @new_date_range.check_out.must_be_kind_of Date
+    end
+    it "Raise an error if check_in or check_out values are not types of Date class" do
+      proc { BookingSystem::DateRange.new("2017-9-15", "2017-9-17") }.must_raise ArgumentError
+    end
+    it "Raise an error if given check_out date is before check_in date" do
+      proc { BookingSystem::DateRange.new(@check_out, @check_in) }.must_raise ArgumentError
     end
   end
 
+  # xdescribe "#valid_dates?" do
+  #   it "Returns true if given check_out date is after check_in date" do
+  #     @new_date_range.valid_dates?.must_equal true
+  #   end
+  #   it "Returns false if given check_out date is before check_in date" do
+  #     date_range = BookingSystem::DateRange.new("17-09-2017", "15-09-2017")
+  #     proc { date_range.valid_dates? }.must_raise ArgumentError
+  #   end
+  # end
+  #
+  # xdescribe "dates_within_range" do
+  #   it "Returns an array" do
+  #     @new_date_range.dates_within_range.must_be_kind_of Array
+  #   end
+  #   it "Returns an array of dates within the given range" do
+  #     @new_date_range.dates_within_range.must_equal [a, b, c]
+  #   end
+  # end
 
 
 
