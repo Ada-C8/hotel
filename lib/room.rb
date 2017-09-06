fd2fb92005fb07e449e755e6c624d6d9fb24c770
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-06 12:28:39 -0700

    Adds room class spec tests and then the logic for rooms class to apass them

--- lib/room.rb
+++ lib/room.rb
@@ -3,7 +3,11 @@ require 'date'
 
 module Hotel
   class Room
-    #room_number
-    #cost
+    attr_reader :room_number, :cost
+
+    def initialize(room_number)
+      @room_number = room_number
+      @cost = 200
+    end
   end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,7 +1,27 @@
 require_relative 'spec_helper'
 
 describe "Room Class" do
- it "" do
-
+  before do
+    @suite1 = Hotel::Room.new(15)
+  end
+ it "can be instantiated" do
+    room = Hotel::Room.new(15)
+    room.must_be_kind_of Hotel::Room
  end
+ it "must respond to room_number and cost" do
+    @suite1.must_respond_to :room_number
+    @suite1.must_respond_to :cost
+ end
+  it "cost must equal 200" do
+    @suite1.cost.must_equal 200
+  end
+  it "cost must be a kind of Integer" do
+    @suite1.cost.must_be_kind_of Integer
+  end
+  it "room number must equal room number" do
+    @suite1.room_number.must_equal 15
+  end
+  it "room number must be a kind of Integer" do
+    @suite1.room_number.must_be_kind_of Integer
+  end
 end
