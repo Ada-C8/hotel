3466be395f2cd93e328fc73d6b0759fc15383a22
Diane Lenertz <lenertzd@grinnell.edu>
2017-09-06 10:13:40 -0700

    add tests for DateRange one_day? and begin and write code to make tests pass

--- lib/daterange.rb
+++ lib/daterange.rb
@@ -4,17 +4,36 @@ module Hotel
 class DateRange
   attr_reader :date1, :date2
 
-  def initialize(date1, *date2)
+  def initialize(date1, date2)
     @date1 = date1
     @date2 = date2
   end
 
-  # def start(dates)
-  #   @start =
-  # end
-  #
-  # def end
-  # end
+  def one_day?
+    if date1 == date2
+      return true
+    else
+      return false
+    end
+  end
+
+  def begin
+    if !one_day? && date1 < date2
+        @beginning = date1
+      elsif !one_day? && date2 < date1
+        @beginning = date2
+      end
+  end
+
+  #def end
+    #if !one_day? && date1 is after date2
+      #@end = date1
+    #elsif !one_day? && date2 is after date1
+      #@end = date2
+  #  end
+    #end
+
+
   #
   # def include?
   # end
--- specs/daterange_spec.rb
+++ specs/daterange_spec.rb
@@ -2,12 +2,13 @@ require_relative 'spec_helper'
 require 'date'
 
 describe 'DateRange' do
-  before do
-    date1 = Date.new(2017,9,12)
-    date2 = Date.new(2017,9,18)
-    @daterange = Hotel::DateRange.new(date1, date2)
-  end
   describe 'initialize' do
+    before do
+      @date1 = Date.new(2017,9,12)
+      @date2 = Date.new(2017,9,18)
+      @date3 = Date.new(2017,9,13)
+      @daterange = Hotel::DateRange.new(@date1, @date2)
+    end
     it 'creates a DateRange object' do
       @daterange.must_be_instance_of Hotel::DateRange
       @daterange.must_respond_to :date1
@@ -15,28 +16,61 @@ describe 'DateRange' do
     end
   end
 
-  describe 'include?' do
-    it 'returns a boolean' do
-      included_date = Date.new(2017,9,13)
-      excluded_date = Date.new(2017,10,12)
-      @daterange.include?(included_date).must_equal true
-      @daterange.include?(excluded_date).must_equal false
+  describe 'one_day?' do
+    before do
+      @date1 = Date.new(2017,9,12)
+      @date2 = Date.new(2017,9,12)
+      @date3 = Date.new(2017,9,13)
+      @daterange = Hotel::DateRange.new(@date1, @date2)
+      @daterange2 = Hotel::DateRange.new(@date1, @date3)
+    end
+    it 'should return true if dates passed are the same' do
+      @daterange.one_day?.must_equal true
     end
 
+    it 'should return false if dates passed are not the same' do
+      @daterange2.one_day?.must_equal false
+    end
 
   end
+
+  describe 'begin' do
+    before do
+      @date1 = Date.new(2017,9,12)
+      @date2 = Date.new(2017,9,18)
+      @daterange = Hotel::DateRange.new(@date1, @date2)
+      @daterange2 = Hotel::DateRange.new(@date2, @date1)
+    end
+    it 'must be a kind of date' do
+      @daterange.begin.must_be_kind_of Date
+    end
+    it 'must identify the correct beginning date' do
+      @daterange.begin.must_equal @date1
+    end
+    it 'must identify the correct beginning date even if passed second' do
+      @daterange2.begin.must_equal @date1
+    end
+  end
+  #
+  # describe 'end' do
+
+  #   daterange.end.must_be_kind_of Date
+  #   @daterange.end(@date1, @date2).must_equal @date2
+  # end
+
+  # describe 'include?' do
+  #   it 'returns a boolean' do
+  #     included_date = Date.new(2017,9,13)
+  #     excluded_date = Date.new(2017,10,12)
+  #     @daterange.include?(included_date).must_equal true
+  #     @daterange.include?(excluded_date).must_equal false
+  #   end
+  # end
 
   describe 'overlap' do
 
   end
 
-  describe 'end' do
-
-  end
-
-  describe 'begin' do
-
-  end
 
 
 end
