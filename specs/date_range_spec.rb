91a6c29fe6b9fcf6c3d2a8b87d69c11af447465d
nkiru onwuneme <generalhrh@gmail.com>
2017-09-06 13:44:32 -0700

    Added test for date_include method

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -4,22 +4,39 @@ require_relative 'spec_helper'
 
 describe "Date Range" do
   describe "initialize method" do
-    it "can be instantiated" do
-      checkin = 2001,2,3
-      checkout = 2001,2,14
-      date =  Hotel::DateRange.new(checkin, checkout)
+    it "takes checkin and checkout dates" do
+      checkin_date = Date.new(2001,2,3)
+      checkout_date = Date.new(2001,2,14)
+      date =  Hotel::DateRange.new(checkin_date, checkout_date)
       date.must_be_instance_of Hotel::DateRange
     end
   end
+  # describe "valid_date" do
+  #   it "checks if the checkin and checkout dates are valid"
+  #     checkin_date = "2001,2,3"
+  #     checkout_date = "2001,2,14"
+  #     date =  Hotel::DateRange.new(checkin_date, checkout_date)
+  #     date.valid_date?(checkin_date).mumust_be_kind_of date
+  # end
 
-  # describe "date include" do
-  #   it "checks if the checkin/checkout dates are included in the date range" do
-  #     checkin = 2001,2,3
-  #     checkout = 2001,2,14
-  #     date_range =  DateRange.new(checkin, checkout)
-  #     date_range.include? ()
-  #
-  #   end
+
+  describe "date include" do
+    it "checks if the checkin/checkout dates are included in the date range" do
+      checkin_date = Date.new(2001,2,3)
+
+      checkout_date = Date.new(2001,2,14)
+
+      date =  Hotel::DateRange.new(checkin_date, checkout_date)
+
+      input_date = Date.new(2001,2,5)
+
+      date.date_include?(input_date) .must_equal true
+    end
+  end
+
+
+
+
     #
     # it "checks if only the checkin date is included in the date range" do
     #
