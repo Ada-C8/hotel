19ae2d8f8a3bb065d83f23b1dc623d0681831fb8
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-09-06 13:02:53 -0700

    Add testing logic to Block_spec

--- lib/Block.rb
+++ lib/Block.rb
@@ -4,6 +4,8 @@ require_relative 'Reservation'
 module Hotel
   class Block
 
+    attr_reader :start_date, :end_date, :discount, :rooms
+
     @@blocks = []
 
     def initialize(start_date, end_date, number_of_rooms, discount = 0.1)
@@ -12,7 +14,6 @@ module Hotel
       @discount = discount
       @rooms = Hotel::Reservation.available(start_date, end_date).sample(number_of_rooms)
       collect_instance
-      print @rooms
     end
 
     def collect_instance
--- specs/Block_spec.rb
+++ specs/Block_spec.rb
@@ -1,12 +1,43 @@
 require_relative 'spec_helper'
 
+module Hotel
+  class Room
+    def self.clear
+      @@rooms = []
+    end
+  end
+
+  class Reservation
+    def self.clear
+      @@reservations = []
+    end
+  end
+end
+
 describe "Block class" do
+
+  before do
+    @min = 1
+    @max = 20
+    (@min..@max).each { |num| Hotel::Room.new(num) }
+  end
+
+  after do
+    Hotel::Room.clear
+    Hotel::Reservation.clear
+  end
 
   it "can create a block with a date range, collection of rooms, and a discounted room rate" do
     proc{Hotel::Block.new}.must_raise Exception
     # the discount is an optional argument, default set to 0.10
     Hotel::Block.new(Date.today, Date.today + 1, 12).must_be_instance_of Hotel::Block
     Hotel::Block.new(Date.today, Date.today + 1, 12, 0.15).must_be_instance_of Hotel::Block
+    x = Hotel::Block.new(Date.today, Date.today + 1, 12, 0.15)
+    x.start_date.must_be_instance_of Date
+    x.end_date.must_be_instance_of Date
+    x.discount.must_be_instance_of Float
+    x.rooms.must_be_instance_of Array
+    x.rooms.each { |room_num| (@min..@max).to_a.include?(room_num).must_equal true }
   end
 
   xit "only makes blocks with rooms available during the specified date range" do
