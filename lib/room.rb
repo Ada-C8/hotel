dbdb16c928e909bf466bc0f4b0d2c1edea56f7db
Gale Harrington <gale.harrington19@gmail.com>
2017-09-06 14:17:46 -0700

    DRY up room_spec.rb

--- lib/room.rb
+++ lib/room.rb
@@ -9,28 +9,19 @@ module Hotel_System
       @reservations = []
     end
 
-    # def available?(check_in, check_out)
-    #   overlap = self.reservations.select do |reservation|
-    #     (check_in...check_out).each do |date|
-    #       (reservation.check_in...reservation.check_out).cover?(date)
-    #     end
-    #   end
-    #   overlap == [] ? true : false
-    # end
-
     def available?(check_in, check_out)
-      @overlap = []
+      overlap = []
       (check_in...check_out).each do |date|
-        @overlap += self.reservations.select do |reservation|
+        overlap += self.reservations.select do |reservation|
           (reservation.check_in...reservation.check_out).cover?(date)
         end
       end
-      @overlap == [] ? true : false
+      overlap == [] ? true : false
     end
 
     def reserve(check_in, check_out)
-      raise ArgumentError.new("Must pass Date object as check_in and check_out") unless check_in.class == Date && check_out.class == Date
-      raise ArgumentError.new("Can not check in and out on same day") unless check_in != check_out
+      raise ArgumentError.new("Must pass Date object as check_in day and check_out day") unless check_in.class == Date && check_out.class == Date
+      raise ArgumentError.new("Cannot check in and out on same day") if check_in == check_out
       raise ArgumentError.new("Time Warp") if (check_out <=> check_in) == -1
       raise ArgumentError.new("Room not available for given date range") unless self.available?(check_in, check_out)
 
@@ -39,6 +30,5 @@ module Hotel_System
       @reservations.push(reservation)
       return reservation
     end
-
   end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -9,7 +9,7 @@ describe 'Room' do
       @check_in = Date.new(2017,9,5)
       @check_out = Date.new(2017,9,7)
     end
-    it "raises an Argument Error if parameters are not Date objects" do
+    it "raises an Argument Error unless passed 2 Date objects" do
       proc {@my_room.reserve("today", "tomorrow")}.must_raise ArgumentError
     end
     it "raises an Argument Error if check_out is earlier than or same day as check_in" do
@@ -24,40 +24,38 @@ describe 'Room' do
       @my_room.reservations.must_be_instance_of Array
       @my_room.reservations.length.must_equal 1
     end
-    it 'raises an error when when a room is not available' do
+    it "raises an error for attempt to reserve already-booked room" do
       @my_room.reserve(@check_in, @check_out)
       proc {@my_room.reserve(@check_in, @check_out)}.must_raise ArgumentError
     end
-    it 'allows you to reserve a room the day someone checks out' do
+    it "allows you to reserve a room the day someone checks out" do
       later_date = Date.new(2017,9,8)
       @my_room.reserve(@check_in, @check_out)
       @my_room.reserve(@check_out, later_date).must_be_instance_of Hotel_System::Reservation
     end
   end
   describe '#available' do
-    it 'returns true if Date argument does not overlap with existing reservation' do
-      birthday_eve = Date.new(1991,7,18)
-      birthday = Date.new(1991,7,19)
-      another_date = Date.new(2017,1,1)
-      later_date = Date.new(2017,7,7)
-      @my_room.available?(birthday_eve, birthday).must_equal true
-      @my_room.available?(another_date, later_date).must_equal true
+    before do
+      @birthday_eve = Date.new(2018,7,18)
+      @birthday = Date.new(2018,7,19)
+      @post_birthday = Date.new(2018,7,20)
+      @date = Date.new(2017,1,1)
+      @later_date = Date.new(2017,7,7)
+      @earlier_date = Date.new(2016,10,10)
+      @even_later_date = Date.new(2017,10,10)
+    end
+    it "returns true if Date argument does not overlap with existing reservation" do
+      @my_room.available?(@birthday_eve, @birthday).must_equal true
+      @my_room.available?(@date, @later_date).must_equal true
     end
     it 'returns false if Date argument does overlap with existing reservation' do
-      birthday_eve = Date.new(1991,7,18)
-      birthday = Date.new(1991,7,19)
-      post_birthday = Date.new(1991,7,20)
-      another_date = Date.new(2017,1,1)
-      later_date = Date.new(2017,7,7)
-      even_later_date = Date.new(2017,10,10)
-      earlier_date = Date.new(2016,10,10)
-      @my_room.reserve(birthday_eve, birthday)
-      @my_room.available?(birthday_eve, birthday).must_equal false
-      @my_room.available?(birthday, post_birthday).must_equal true
-      @my_room.reserve(another_date, later_date)
-      @my_room.available?(earlier_date, later_date).must_equal false
-      @my_room.available?(another_date, even_later_date).must_equal false
-      @my_room.available?(earlier_date, even_later_date).must_equal false
+      @my_room.reserve(@birthday_eve, @birthday)
+      @my_room.available?(@birthday_eve, @birthday).must_equal false
+      @my_room.available?(@birthday, @post_birthday).must_equal true
+      @my_room.reserve(@date, @later_date)
+      @my_room.available?(@earlier_date, @later_date).must_equal false
+      @my_room.available?(@date, @even_later_date).must_equal false
+      @my_room.available?(@earlier_date, @even_later_date).must_equal false
     end
   end
 end
