42822fa6e2adf30ddbc89f213c9877ba0a219e1e
averikitsch <aekitsch@gmail.com>
2017-09-06 09:40:01 -0700

    updated room class availability methods

--- lib/hotel-room.rb
+++ lib/hotel-room.rb
@@ -14,17 +14,38 @@ module Hotel
       end
     end
 
-    def self.all
-      @@hotel_rooms = [] #NOTE should this be an class variable?
+    def self.list_all
+      hotel_rooms = [] #NOTE should this be an class variable?
       NUMBER_OF_ROOMS.times do |i|
-        @@hotel_rooms << self.new(i+1)
+        hotel_rooms << self.new(i+1)
       end
-      return @@hotel_rooms
+      return hotel_rooms
     end
 
-    def self.list_all
-      #NOTE: may be repeat of self.all, do I need to recreate everytime I want to show a list?
-      return @@hotel_rooms
+    # def self.list_all
+    #   #NOTE: may be repeat of self.all, do I need to recreate everytime I want to show a list?
+    #   return @@hotel_rooms
+    # end
+
+    def available?(start_date, end_date)
+      rooms_available = self.class.all_available_rooms(start_date, end_date)
+      rooms_available.each do |room|
+        if room.number == self.number
+          return true
+        end
+      end
+      return false
+    end
+
+    def self.all_available_rooms(start_date, end_date)
+      available_rooms = list_all
+      (start_date...end_date).each do |date|
+        list = Hotel::Reservation.list_for_date(date)
+        list.each do |reservation|
+          available_rooms.delete_at(reservation.room.number-1)
+        end
+      end
+      return available_rooms
     end
 
   end
--- specs/hotel-room_specs.rb
+++ specs/hotel-room_specs.rb
@@ -19,21 +19,33 @@ describe "Hotel::Room" do
     end
 
     it "can make a hotel of rooms" do
-      hotel = Hotel::Room.all
+      Hotel::Room.must_respond_to :list_all
+      hotel = Hotel::Room.list_all
       hotel.must_be_instance_of Array
       20.times do |i|
         hotel[i].must_be_instance_of Hotel::Room
       end
     end
 
-    it "can make a list of rooms" do
-      Hotel::Room.must_respond_to :list_all
-      hotel = Hotel::Room.all
-      Hotel::Room.list_all.must_be_instance_of Array
-      hotel_list = Hotel::Room.list_all
-      20.times do |i|
-        hotel_list[i].must_be_instance_of Hotel::Room
-      end
+    # it "can make a list of rooms" do
+    #   Hotel::Room.all #make hotel
+    #   Hotel::Room.list_all.must_be_instance_of Array
+    #   hotel_list = Hotel::Room.list_all
+    #   20.times do |i|
+    #     hotel_list[i].must_be_instance_of Hotel::Room
+    #   end
+    # end
+
+    it "can list rooms that are available" do
+      start_date = Date.new(2017,9,5)
+      end_date = Date.new(2017,9,8)
+      start_date2 = Date.new(2017,9,5)
+      end_date2 = Date.new(2017,9,8)
+      # room_reservation = Hotel::Reservation.new(start_date,end_date,Hotel::Room.new(1))
+      # room_reservation2 = Hotel::Reservation.new(start_date,end_date,Hotel::Room.new(2))
+      all_rooms = Hotel::Room.all_available_rooms(start_date, end_date)
+      all_rooms.must_be_instance_of Array
+      all_rooms.length.must_equal 20
     end
 
   end
