53223c5c41347e27fbaf658fe1e02babdca4bdb2
Eva Sawyer <esawyer25@gmail.com>
2017-09-05 21:57:24 -0700

    set up initialize

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,12 +1,36 @@
+require_relative 'reservation'
 module My_Hotel
   class Hotel
+    ROOMS = {room_1: 200,
+      room_2: 200,
+      room_3: 200,
+      room_4: 200,
+      room_5: 200,
+      room_6: 200,
+      room_7: 200,
+      room_8: 200,
+      room_9: 200,
+      room_10: 200,
+      room_11: 200,
+      room_12: 200,
+      room_13: 200,
+      room_14: 200,
+      room_15: 200,
+      room_16: 200,
+      room_17: 200,
+      room_18: 200,
+      room_19: 200,
+      room_20: 200
+    }
 
-    attr_reader :list_of_rooms, :list_of_reservations
+
+    attr_reader :rooms, :list_of_reservations
 
     def initialize
-      @list_of_rooms = make_rooms
-      @list_of_reservations = Reservation.all
+      @rooms = ROOMS
+      @list_of_reservations = []
     end
+
 
 
     # def make_rooms
@@ -18,30 +42,33 @@ module My_Hotel
     #   end
     # end
 
-# def get_reservations
-# end
 
-    # def display_rooms
-    #   #puts out list of rooms
-    # end
+    def display_rooms
+      index = 1
+      rooms.each do |room, price|
+        puts "Room Number #{index}: $#{price} per night."
+        index += 1
+      end
 
-    # def display_reservations
-    #   #puts out list of reservations
-    # end
+      # def display_reservations
+      #   #puts out list of reservations
+      # end
 
-#     def rooms_available_date(start_date, end_date)
-# #display rooms available on that date
-#     end
+      #     def rooms_available_date(start_date, end_date)
+      # #display rooms available on that date
+      #     end
 
-#def make_block
-#define a block
-# make a random_id_number for block.
-#end
+      #def make_block
+      #define a block
+      # make a random_id_number for block.
+      #end
 
-#def rooms_available_block(block_id)
-#shows if any rooms in the block are available
-# end
+      #def rooms_available_block(block_id)
+      #shows if any rooms in the block are available
+      # end
 
-
+    end
   end
 end
+# h= My_Hotel::Hotel.new
+# h.display_rooms
