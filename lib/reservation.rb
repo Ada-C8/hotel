f6859ad8ed76af98ae2fd490a2e1b294eb2585d4
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-09-05 21:41:20 -0700

    Added tests for initialize method for reservation class and passed some of them

---
+++ lib/reservation.rb
@@ -0,0 +1,25 @@
+require_relative 'date_range'
+
+module BookingSystem
+  class Reservation
+
+    @@id_count = 1
+    COST = 200
+
+    attr_reader :id, :date_range, :room, :total_cost
+
+    def initialize(date_range)
+      @id = @@id_count
+      @@id_count += 1
+      @date_range = date_range
+      @room = room
+      @total_cost = COST * date_range.dates_within_range.length
+
+    end #end of initialize
+
+
+
+
+  end #end of class
+
+end #end of module
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -6,15 +6,29 @@ describe "Reservation" do
     @check_out = Date.new(2017,9,17)
     @date_range = BookingSystem::DateRange.new(@check_in, @check_out)
     @new_reservation = BookingSystem::Reservation.new(@date_range)
+    @another_reservation = BookingSystem::Reservation.new(@date_range)
   end
 
   describe "#initialize" do
     it "ID should be an integer" do
       @new_reservation.id.must_be_kind_of Integer
     end
-    it "" do
-      
+    it "ID should be 1 for first reservation" do
+      @new_reservation.id.must_equal 1
     end
+    it "ID should be 2 for second reservation" do
+      @another_reservation.id.must_equal 2
+    end
+    it "Date_range can be called by .date_range" do
+      @new_reservation.date_range.must_equal @date_range
+    end
+    it "Total cost should be an integer" do
+      @new_reservation.total_cost.must_be_kind_of Integer
+    end
+    it "Returns the rigth total cost" do
+      @new_reservation.total_cost.must_equal 400
+    end
+
   end
 
 end #end of discribe
