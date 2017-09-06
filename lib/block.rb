f58da3374f1283dab03ed8613f0f2ef0594359c1
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-09-06 16:02:30 -0700

    Added class block and test for initialize method

--- lib/block.rb
+++ lib/block.rb
@@ -1,5 +1,23 @@
 module BookingSystem
  class Block
 
+   COST = 200
+   DISCOUNT = 0.9
+   @@id_count = 1
+
+   attr_reader :date_range, :number_of_rooms, :id, :block_cost
+
+   def initialize(date_range, number_of_rooms)
+     @id = @@id_count
+     @@id_count += 1
+     @date_range = date_range
+     @rooms = []
+     @block_cost = (COST * DISCOUNT * date_range.dates_within_range.length * number_of_rooms).to_i
+
+   end #end of initialize
+
+
+
+
  end #end of class
 end #end of module
--- specs/block_spec.rb
+++ specs/block_spec.rb
@@ -1,1 +1,27 @@
 require_relative 'spec_helper'
+
+describe "Block" do
+  before do
+    @number_of_rooms = 3
+    @check_in = Date.new(2017,9,15)
+    @check_out = Date.new(2017,9,17)
+    @date_range = BookingSystem::DateRange.new(@check_in, @check_out)
+    @new_block = BookingSystem::Block.new(@date_range, @number_of_rooms)
+  end
+  describe "#initialize" do
+    it "ID should be an integer" do
+      @new_block.id.must_be_kind_of Integer
+    end
+    it "Date_range can be called by .date_range" do
+      @new_block.date_range.must_equal @date_range
+    end
+    it "Total cost should be an integer" do
+      @new_block.block_cost.must_be_kind_of Integer
+    end
+    it "Returns the rigth total cost" do
+      @new_block.block_cost.must_equal 1080
+    end
+
+  end
+
+end #end of describe
