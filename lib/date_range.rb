bffecd255362af90941817f404a323a0686d30f8
Tanisha <tanishahampden@gmail.com>
2017-09-06 13:32:08 -0700

    add tests that verify Date Range instance variables

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,10 +1,13 @@
 require 'date'
 module Hotel
   class DateRange
+    attr_reader :start_date, :end_date, :number_of_nights
     def initialize(start_date, end_date)
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
       @number_of_nights = (@end_date - @start_date).to_i
     end
+
+
   end
 end
--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -8,5 +8,17 @@ describe "DateRange Class" do
     it "DateRange.new is an instance of Hotel module" do
       @date_range_test.must_be_instance_of Hotel::DateRange
     end
+    it "Responds to start_date variable" do
+      @date_range_test.must_respond_to :start_date
+    end
+    it "Responds to end_date variable" do
+      @date_range_test.must_respond_to :end_date
+    end
+    it "Responds to number_of_nights variable" do
+      @date_range_test.must_respond_to :number_of_nights
+    end
+    it "Equals number_of_nights" do
+      @date_range_test.number_of_nights.must_equal 3
+    end
   end
 end
