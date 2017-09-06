9b1783ae0221ea482c1c4c7a35c75cb7ac6cde21
Amy Lee <amy.yj.lee@gmail.com>
2017-09-06 15:44:29 -0700

    get a list of all reservations on a certain date; wrote basic functionality and basic tests; passing

--- lib/Booking.rb
+++ lib/Booking.rb
@@ -1,15 +1,61 @@
 ### Booking Program class that holds all of the business logic for the hotel and the reservation system
+require 'date'
+require 'pry'
+require_relative 'Reservation'
+require_relative 'Room'
 
 module Hotel
   # NUMBER_OF_ROOMS = 20  ### might not want to use constant, especially if we have subclasses of rooms (by type), and each room subclass might have a different number. may best be a constant defined by class/subclass
 
   class BookingProgram
-    #rooms = Hotel::Room.all # returns an array or hash of Room objects
+    attr_reader :all_rooms, :all_reservations
 
-    #reservations = Hotel::Reservation.all #returns an array or hash of Reservation Objects
+    def initialize # do we want to initialize with hotel name?
+      @all_rooms = Hotel::Room.all # returns an array or hash of Room objects
+      @all_reservations = []
 
-  #  puts " rooms: #{rooms} \n reservations: #{reservations}"
+    end
 
+    def make_reservation(guest_id, check_in,check_out,room_id)
+
+      reservation = Hotel::Reservation.new(guest_id, check_in,check_out,room_id)
+      reservation.id += 1 #something
+
+      reservation.room.reserve_room(check_in,check_out,reservation.id, guest_id)
+
+      @all_reservations << reservation
+
+    end
+
+    def find_res_by_date(date_str)
+      date_object= Date.parse(date_str)
+      reserved_on_date = []
+      @all_reservations.each {|reservation| reserved_on_date << reservation if reservation.room.all_dates.include?(date_object)}
+
+      return reserved_on_date
+
+      #self.all.select {|reservation| reservation.room.all_dates.include?(date_object)}
+    end
 
   end
+
 end
+
+
+# binding.pry
+
+#########WAVE 2 #####
+
+# def available_rooms(check_in,check_out)
+#   # confirm check_out date availability logic
+#   available_rooms  = []
+#
+#   candidates = @all_rooms.select {|room| room.available?(check_in) && room.available?(check_out)}
+#
+#   candidates.each do |room|
+#     available_rooms << room if room.available_all_days?(check_in, check_out)
+#   end
+#
+#   return available_rooms if available_rooms.count > 0
+#   return false
+# end
--- specs/Booking_spec.rb
+++ specs/Booking_spec.rb
@@ -1,1 +1,41 @@
 require_relative 'spec_helper'
+
+describe "BookingProgram" do
+  describe "Initialize" do
+    it "initializes with 20 rooms, numbered 1 through 20 and I can can access the list of all rooms " do
+      new_hotel = Hotel::BookingProgram.new
+
+      new_hotel.all_rooms.must_be_instance_of Array
+      new_hotel.all_rooms.each do |room|
+        room.must_be_instance_of Hotel::Room
+      end
+
+
+    end
+
+  end
+
+  describe "find_res_by_date" do
+    it "Returns an array of Reservation objects" do
+      new_hotel =  Hotel::BookingProgram.new
+
+      new_hotel.make_reservation(1, "2017-03-14", "2017-04-16", 3)
+
+      new_hotel.make_reservation(2, "2017-03-14", "2017-04-16", 5)
+
+      new_hotel.make_reservation(3, "2017-02-02", "2017-02-08", 18)
+
+      new_hotel.make_reservation(5, "2017-12-08", "2017-12-10", 7)
+
+      new_hotel.make_reservation(4, "2017-03-21", "2017-03-22", 15)
+
+
+      new_hotel.find_res_by_date("2017-03-21").must_be_instance_of Array
+
+      new_hotel.find_res_by_date("2017-03-21").count.must_equal 3
+
+      p new_hotel.find_res_by_date("2017-03-21")
+
+    end
+  end
+end
