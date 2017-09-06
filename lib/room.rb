92119cf5498145ed5a22e22a4dda877411c6dd4b
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-06 04:28:26 -0700

    Changed implementation of Room's self.create_rooms (previously self.all) to use Arrays instead of Hashes. Added Reservation class.

--- lib/room.rb
+++ lib/room.rb
@@ -1,25 +1,27 @@
 require 'date'
 
 module Hotel
-  NUM_OF_ROOMS = 20
-  class Room
-    attr_reader :available, :check_in_date, :check_out_date, :cost
 
-    def initialize
+  NUM_OF_ROOMS = 20
+
+  class Room
+    attr_reader :available, :check_in_date, :check_out_date, :cost, :id
+
+    def initialize(room_number)
       @available = true
       # @blocked_off = false #for Wave 3
       @check_in_date = nil
       @check_out_date = nil
       @cost = 200
+      @room_number = room_number
     end
 
-    def self.all
-      room_hash = {}
+    def self.create_room
+      room_array = []
       NUM_OF_ROOMS.times do |i|
-        current_room_hash = {i+1 => Hotel::Room.new} # Hotel Room Numbers start with 1, not 0
-        room_hash.merge!(current_room_hash)
+        room_array << Hotel::Room.new(i + 1) # Hotel Room Numbers start with 1, not 0
       end
-      return room_hash
+      return room_array
     end
 
     def change_availability
