aa8219a4bebb24448625d8b92129f1076bae2cda
Tanisha <tanishahampden@gmail.com>
2017-09-06 12:20:10 -0700

    add test for check DateRange initialize

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,9 +1,9 @@
-require 'Date'
-module HotelClass
+require 'date'
+module Hotel
   class DateRange
     def initialize(start_date, end_date)
-      @start_date = Date.new(start_date)
-      @end_date = Date.new(end_date)
+      @start_date = Date.parse(start_date)
+      @end_date = Date.parse(end_date)
       @number_of_nights = (@end_date - @start_date).to_i
     end
   end
--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -1,1 +1,12 @@
 require_relative 'spec_helper'
+
+describe "DateRange Class" do
+  before do
+    @date_range_test = Hotel::DateRange.new("2017-9-12", "2017-9-15")
+  end
+  describe "Initialize DateRange Class" do
+    it "DateRange.new is an instance of Hotel module" do
+      @date_range_test.must_be_instance_of Hotel::DateRange
+    end
+  end
+end
