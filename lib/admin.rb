5271d682823a4ce6ec69fa286d4259dbf9103d55
Roxanne <argonkryptonneon@gmail.com>
2017-09-06 16:37:56 -0700

    figuring out mixins

--- lib/admin.rb
+++ lib/admin.rb
@@ -1,13 +1,14 @@
 # require_relative 'hotel'
 require_relative 'reservation'
+require 'date'
 
 module Hotel
 
   class Admin
-    attr_reader :reservations_list
+    attr_reader :reservations_list, :all_rooms
 
     def initialize
-      @reservations_list = {}
+      @reservations_list = []
       @all_rooms = []
       make_hotel_rooms
     end
@@ -18,29 +19,40 @@ module Hotel
       end
     end
 
-    def reserve(check_in, check_out, room, name, contact_info)
+
+      def get_date_range(check_in, check_out)
+        return  (Date.new(check_in) ... Date.new(check_out)).to_a
+      end
+
+    def self.reserve(check_in, check_out, room, name, contact_info)
       #checks that the date hash't been previously reserved
 
       raise ArgumentError.new("This room is already reserved on these days") if rooms_available?(check_in, check_out).include?(room) == false
-      new_reservation = Hotel::Reservation.new(check_in, check_out, room, name, contact_info)
+      new_reservation = Hotel::Reservation.new(check_in, nights, room, name, contact_info)
 
       @reservations_list << new_reservation
 
 
     end#reserve
 
-    def rooms_available?(check_in, check_out)
-      #searches through the reservation list to find available rooms
-      #not sure if this will work  the .each.each is a little crazy TODO check for some sort of sweeeeeet enumerable to use on this bad boy.
+    def self.rooms_available?(check_in, check_out)
+      # #searches through the reservation list to find available rooms
+      # #not sure if this will work  the .each.each is a little crazy TODO check for some sort of sweeeeeet enumerable to use on this bad boy.
+      wanted_dates = get_date_range(check_in, check_out)
       reserved_rooms_for_dates = []
-      @reservations_list[check_in, check_out].each do |reservation|
-        #needs to check if the room is included in this reservation on the day
+      case
+      when @reservations_list = []
+        return @all_rooms
+      else
+        @reservations_list.each do |reservation|
+          #needs to check if the room is included in this reservation on the day
 
-        overlap =  reservation.dates_reserved & [check_in .. check_out]
-        overlap.any? ? false : reserved_rooms_for_dates << reservation.room
+          overlap =  reservation.nights_reserved & wanted_dates
+          overlap.any? ? reserved_rooms_for_dates << reservation.room : false
 
+        end
       end
-      rooms_available = @all_rooms - reserved_rooms_for_dates
+    return @all_rooms - reserved_rooms_for_dates
     end#rooms_available
 
   end #class Admin
--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,27 +1,39 @@
 # require_relative 'hotel'
 require_relative 'room'
 require 'date'
+require 'pry'
 
 module Hotel
-  PRICE_PER_NIGHT = 200
+PRICE_PER_NIGHT = 200
+
+  def Hotel.get_date_range(check_in, check_out)
+    return  (Date.new(check_in) ... Date.new(check_out)).to_a
+  end
 
   class Reservation
 
-    attr_accessor :date_range, :room, :name, :contact_info, :price
+    attr_accessor :date_range, :room, :name, :contact_info, :check_in, :check_out
 
-    def  initialize(date_range, room, name, contact_info)
-      @date_range = date_range
+    def  initialize(check_in, check_out, room, name, contact_info)
+
+      @check_in = Date.new(check_in)
+      @check_out = Date.new(check_out)
+      @date_range = get_date_range(check_in, check_out)
       @room = room
       @name = name
       @contact_info = contact_info
-      @price = date_range.length * PRICE_PER_NIGHT
+      @price = (@date_range.length) * PRICE_PER_NIGHT
     end
 
-    def dates
-      dates_reserved = []
-      @date_range.each_entry{|date| dates_reserved << date}
-      dates_reserved
+
+      def get_date_range(check_in, check_out)
+        return  (Date.new(check_in) ... Date.new(check_out)).to_a
+      end
+      
+    def self.nights_reserved
+      @date_range
     end #end available?
 
   end#end class
+    # binding.pry
 end#end module
