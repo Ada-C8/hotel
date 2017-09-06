4a106c7eab30c6c463f10c74ecdfc7a3f1e0be0d
Eva Sawyer <esawyer25@gmail.com>
2017-09-06 07:30:44 -0700

    added an overlap? method

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,25 +1,32 @@
+
 require 'date'
 module My_Hotel
   class Date_Range
 
-    attr_reader :start_date, :end_date, :date_range
+    attr_reader :arrive, :leave, :nights
 
-    def initialize(start_date, end_date)
-      @start_date = Date.civil(start_date[0], start_date[1], start_date[2])
-      @end_date = Date.civil(end_date[0], end_date[1], end_date[2])
-      @date_range = [@start_date, @end_date]
+    def initialize(arrive, leave)
+      @arrive = Date.civil(arrive[0], arrive[1], arrive[2])
+      @leave = Date.civil(leave[0], leave[1], leave[2])
+      @nights = (@arrive..(@leave.prev_day))
     end
 
 
 
     # def valid_dates?
-  # end
-    #
-    # def overlab?
-    #   #is the start_date before the end_date?
     # end
-    #
+
+    #is the start_date before the end_date?
+    #accepts date as a string [year, month, day]
+    def overlap?(input_date)
+      input_date = Date.civil(input_date[0], input_date[1], input_date[2])
+      @nights.include?(input_date)
+    end
+
 
 
   end
 end
+
+
+#h = My_Hotel::Date_Range.new([2017,1,2], [2017,1,5])
