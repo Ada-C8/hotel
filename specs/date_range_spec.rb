3e6c923b3f9304657a9447fb8446c7d5dbd9c672
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-09-06 11:23:15 -0700

    implement ::range_with method

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -28,9 +28,35 @@ describe 'DateRange' do
       dates = DateRange::range_to(@before, @after)
 
       dates.first.strftime.must_equal '2017-10-01'
-      dates.last.strftime.must_equal '2017-10-13'      
+      dates.last.strftime.must_equal '2017-10-13'
     end
   end
+
+  describe 'self.range_with' do
+    it 'raises ArgumentError if start date is after end date' do
+      proc {
+        DateRange::range_with(@after,@before)
+      }.must_raise ArgumentError
+    end
+
+    it 'returns an array of Dates' do
+      dates = DateRange::range_with(@before, @after)
+
+      dates.must_be_kind_of Array
+
+      dates.each do |date|
+        date.must_be_kind_of Date
+      end
+    end
+
+    it 'includes all dates up to AND including the end date' do
+      dates = DateRange::range_with(@before, @after)
+
+      dates.first.strftime.must_equal '2017-10-01'
+      dates.last.strftime.must_equal '2017-10-14'
+    end
+  end
+
 
   describe 'validate' do
     it 'returns input unchanged if input is a Date object' do
