6c38487174450268a2ff0fa548f324bf69e208aa
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-09-05 21:47:38 -0700

    pseudocode remaining methods

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -16,9 +16,44 @@ module Hotel
       list
     end
 
-    # #view_avail()
-    # #view_booked()
-    # #is_available?()
-    # #make_reservation()
+    def make_reservation(checkin, checkout)
+      @reservations << Reservation.new(checkin, checkout)
+    end
+
+    # def view_avail(checkin, checkout)
+    #   available_rooms = []
+    #   @rooms.each do |room|
+    #     available_rooms << room.number if room.is_avail?(checkin, checkout)
+    #     available_rooms
+    #   end
+    # end
+
+    # def view_reservations(date)
+    #   reservations = []
+    #   @reservations.each do |reservation|
+    #     reservations << reservation if reservation.dates.include?(date)
+    #   end
+    #   reservations
+    # end
+
+    # def find_available_rooms(checkin, checkout)
+    #   booked_rooms, available_rooms = [], []
+    #   @reservations.each do |reservation|
+    #     if reservation.room is not in booked_room && !(reservation.includes_dates?(checkin,checkout))
+    #       booked_rooms << reservation.number
+    #     end
+    #   end
+    #   @rooms.each do |room|
+    #     available_rooms << room unless booked_rooms.include?room.number
+    #   end
+    #   available_rooms
+    # end
+
+
+    # def make_reservation(checkin, checkout)
+    #   room_num = find_available_rooms(checkin, checkout).first
+    #   @reservations << Reservation.new(room_num, checkin, checkout)
+    # end
+
   end
 end
