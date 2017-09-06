131a909381ddd9773328910648b31807f6954f1f
nkiru onwuneme <generalhrh@gmail.com>
2017-09-06 13:42:36 -0700

    Created date_include method

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -5,10 +5,31 @@ module Hotel
   class DateRange
     attr_reader :checkin, :checkout
 
-    def initialize(checkin, checkout)
-      @checkin = checkin
-      @checkout = checkout
+# Checkin and checkout dates are instances of the Date class
+    def initialize(checkin_date, checkout_date)
+      @checkin_date = checkin_date
+      # return checkin_date if  Date.valid_date?(checkin_date)
+      @checkout_date = checkout_date
+      # return checkout_date if Date.valid_date(checkout_date)
     end
-  end #end of class
 
+# Check if date is included in date range
+    def date_include?(date)
+      if date >= @checkin_date && date <= @checkout_date
+        return true
+      else
+        return false
+        # raise ArgumentError.new("#{date} is an invalid date!")
+      end
+    end
+
+  end #end of class
 end #end of module
+
+
+#### TESTING ##########
+
+# checkin_date = 2001,2,3
+# checkout_date = 2001,2,14
+# date =  Hotel::DateRange.new(checkin_date, checkout_date)
+# puts date.date_include?(2001,2,5)
