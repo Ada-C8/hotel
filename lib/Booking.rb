366a37e252e07f4bc22c3e784cb33af56c356722
Kimberley Zell <kimberley.zell@gmail.com>
2017-09-06 13:13:17 -0700

    created DateRange class file

--- lib/Booking.rb
+++ lib/Booking.rb
@@ -1,14 +1,16 @@
 require 'date'
+require 'DateRange'
 
 module Hotel
   class Booking
     def initialize(check_in, check_out) #room_rate, total_cost)
-      # @check_in_date = check_in_date
-      # @check_out_date = check_out_date
+
+      @check_in = Date.new(check_in)
+      @check_out = Date.new(check_out)
       #@date_range = date_range
       #@room_rate = room_rate
       @dates = DateRange.new(check_in, check_out)
-      @total_cost = total_cost
+      #@total_cost = total_cost
     end
   end
 
---
+++ lib/DateRange.rb
@@ -0,0 +1,8 @@
+require 'date'
+
+module Hotel
+  class DateRange
+    def initialize(check_in, check_out)
+    end
+  end
+end
