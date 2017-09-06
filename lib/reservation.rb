2382fa0cfab3cb968eaa8b6f776b9c04d49e8d07
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-09-06 11:40:59 -0700

    implement @id and #create_id

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -2,19 +2,26 @@ module Hotel
   class Reservation
     require_relative 'date_range'
     # @reservation_id
-    attr_reader :total_cost, :dates
+    attr_reader :total_cost, :dates, :checkin, :checkout, :id
 
     def initialize(room, checkin, checkout)
       @room = room
-      checkin = Date.parse(checkin)
-      checkout = Date.parse(checkout)
-      @dates = DateRange::range_to(checkin, checkout)
+      @checkin = Date.parse(checkin)
+      @checkout = Date.parse(checkout)
+      @dates = DateRange::range_to(@checkin, @checkout)
+      @id = create_id
       get_total
     end
 
     def get_total
       num_nights = @dates.length
       # @total_cost = num_nights * @room.cost
+    end
+
+    private
+
+    def create_id
+      '%.2d%.2d%.4d' % [@checkin.month, @checkin.day, rand(9999)]
     end
 
     # def includes_dates?(checkin, checkout)
