8f259ccb3a3407e86f5f98608cbe800a1eaae175
Andrea Chen <andreachen.ac@gmail.com>
2017-09-06 12:36:15 -0700

    Add error handling in Reservation for arguments, add constant for room price. Create respective tests, all passing.

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,10 +1,26 @@
 module BookingSystem
   class Reservation
+
+    class InvalidRoomError < StandardError
+    end
+
     attr_reader :reservation_dates, :room
 
+    ROOM_PRICE = 200
+
     def initialize(room, check_in, check_out)
+
+      if room.class != Integer
+        raise InvalidRoomError.new("Room number must be entered as a digit (e.g., 1, 2, 3...). Input given: #{room}")
+      end
+
+      if room > 20 || room < 1
+        raise InvalidRoomError.new("Room number must be between 1 and 20. Input given: #{room}")
+      end
+
       @room = room
       @reservation_dates = DateRange.new(check_in, check_out).all_reservation_dates
+
 
     end
 
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -8,20 +8,37 @@ describe "Reservation class" do
     @test_ob = BookingSystem::Reservation.new(1, check_in, check_out)
   end
 
+  it "Has a ROOM_PRICE constant that is assigned to the integer 200" do
+    BookingSystem::Reservation::ROOM_PRICE.must_equal 200
+  end
+
   describe "initialize method" do
 
     it "Can be instantiated" do
       @test_ob.must_be_instance_of BookingSystem::Reservation
     end
 
-    it "Has an instance variable @reservation_dates which is an array of Date objects" do
+    it "Has an instance variable @reservation_dates which is an Array of Date objects" do
       @test_ob.reservation_dates.must_be_instance_of Array
       @test_ob.reservation_dates[0].must_be_instance_of Date
     end
 
-    it "Has an instance variable @room which is an integer" do
+    it "Has an instance variable @room which is an Integer" do
       @test_ob.room.must_be_instance_of Integer
     end
+
+    it "Raises InvalidRoomError if room argument is not an Integer between 1-20" do
+      check_in = Date.new(2017,9,12)
+      check_out = Date.new(2017,9,12)
+
+      proc { BookingSystem::Reservation.new("one", check_in, check_out) }.must_raise BookingSystem::Reservation::InvalidRoomError
+
+      proc { BookingSystem::Reservation.new(0, check_in, check_out) }.must_raise BookingSystem::Reservation::InvalidRoomError
+
+      proc { BookingSystem::Reservation.new(21, check_in, check_out) }.must_raise BookingSystem::Reservation::InvalidRoomError
+    end
+
+
 
   end
 
