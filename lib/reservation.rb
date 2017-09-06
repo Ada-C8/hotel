fba8769410ca4d31eef7eb7f06efd229a395b589
Roxanne <argonkryptonneon@gmail.com>
2017-09-06 12:04:29 -0700

    added method to pull out date array

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,20 +1,25 @@
 require_relative 'hotel'
+require_relative 'room'
+require 'date'
 
 module Hotel
+  PRICE_PER_NIGHT = 200
 
   class Reservation
 
-    attr_accessor :date_range, :room, :name, :contact_info
+    attr_accessor :date_range, :room, :name, :contact_info, :price
 
     def  initialize(date_range, room, name, contact_info)
-      @date = date_range
+      @dates = date_range
       @room = room
       @name = name
       @contact_info = contact_info
+      @price = date_range.length * PRICE_PER_NIGHT
     end
 
-    def available?(date)
-      @date == date ? false : true
+    def dates
+      dates_reserved = []
+      dates_reserved << @dates.each
     end #end available?
 
   end#end class
