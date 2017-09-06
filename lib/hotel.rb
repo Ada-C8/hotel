0540cac3433a69c47d28de820122f79ee56feab5
Andrea Chen <andreachen.ac@gmail.com>
2017-09-06 16:58:40 -0700

    Add methods to Hotel class (make_reservation, reservations_for_specific_date, available_rooms, room_available?), and corresponding tests. All passing.

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,5 +1,9 @@
 module BookingSystem
   class Hotel
+
+    class UnavailableRoomError < StandardError
+    end
+
     attr_reader :rooms, :all_reservations
     def initialize
       @rooms = (1..20).to_a
@@ -7,13 +11,67 @@ module BookingSystem
     end
 
     def make_reservation(room, check_in, check_out)
-      reservation = BookingSystem::Reservation.new(room, check_in, check_out)
-      @all_reservations << reservation
-      return reservation
+      #call room_available? method
+      room_available = room_available?(room, check_in, check_out)
+
+      if room_available == true
+        reservation = BookingSystem::Reservation.new(room, check_in, check_out)
+        @all_reservations << reservation
+
+        return reservation
+      else #false
+        raise UnavailableRoomError.new("Room #{room} is not available for those dates - #{check_in.to_s} to #{check_out.to_s}")
+
+      end
     end
 
-    def available_rooms
-      return []
+    def reservations_for_specific_date(date)
+      reservations_for_specific_date = []
+      @all_reservations.each do |res|
+        if res.reservation_dates.include?(date)
+          reservations_for_specific_date << res
+        end
+      end
+
+      return reservations_for_specific_date
+    end
+
+    def available_rooms(check_in, check_out)
+      available_rooms = (1..20).to_a
+      requested_date_range = DateRange.new(check_in, check_out).all_reservation_dates
+
+      @all_reservations.each do |res|
+        same_dates = requested_date_range & res.reservation_dates
+        if same_dates.any?
+          available_rooms.delete(res.room)
+        end
+      end
+
+      return available_rooms
+    end
+
+    private
+
+    #check if room is available for selected dates
+    def room_available?(room, check_in, check_out)
+      all_res_for_room = @all_reservations.select {|res| res.room == room}
+
+      requested_date_range = DateRange.new(check_in, check_out).all_reservation_dates
+
+      all_dates_for_room = []
+      all_res_for_room.each do |res|
+        all_dates_for_room += res.reservation_dates
+      end
+
+      #compare all reservation dates for the room with the requested date range
+      same_dates = requested_date_range & all_dates_for_room
+
+      if same_dates.length > 0
+        return false
+      end
+
+      return true
+
     end
 
   end
--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -2,6 +2,14 @@ require_relative 'spec_helper'
 
 describe "Hotel class" do
 
+  describe "UnavailableRoomError class" do
+
+    it "Can be instantiated" do
+      BookingSystem::Hotel::UnavailableRoomError.new.must_be_instance_of BookingSystem::Hotel::UnavailableRoomError
+    end
+
+  end
+
   before do
     @test_ob = BookingSystem::Hotel.new
   end
@@ -27,18 +35,6 @@ describe "Hotel class" do
 
   end#initialize
 
-  describe "available_rooms method" do
-
-    it "Can be called" do
-      @test_ob.must_respond_to :available_rooms
-    end
-
-    it "Returns an Array" do
-      @test_ob.available_rooms.must_be_instance_of Array
-    end
-
-  end
-
   describe "make_reservation method" do
 
     before do
@@ -60,6 +56,83 @@ describe "Hotel class" do
       @test_ob.all_reservations.length.must_equal 1
     end
 
+    it "Raises an UnavailableRoomError if room is unavailable" do
+      @test_ob.make_reservation(@room, @check_in, @check_out)
+
+      proc { @test_ob.make_reservation(@room, @check_in, @check_out) }.must_raise BookingSystem::Hotel::UnavailableRoomError
+    end
+
+  end
+
+  describe "reservations_for_specific_date" do
+
+    before do
+      @room = 1
+      @check_in = Date.new(2017,9,9)
+      @check_out = Date.new(2017,9,12)
+    end
+
+    it "Can be called" do
+      @test_ob.must_respond_to :reservations_for_specific_date
+    end
+
+    it "Returns an array of Reservation objects" do
+      @test_ob.make_reservation(@room, @check_in, @check_out)
+
+      @test_ob.make_reservation(2, Date.new(2017,9,11), Date.new(2017,9,15))
+
+      @test_ob.reservations_for_specific_date(Date.new(2017,9,10)).must_be_instance_of Array
+
+      @test_ob.reservations_for_specific_date(Date.new(2017,9,10))[0].must_be_instance_of BookingSystem::Reservation
+
+      @test_ob.reservations_for_specific_date(Date.new(2017,9,11)).length.must_equal 2
+    end
+  end
+
+  describe "available_rooms method" do
+
+    before do
+      @room = 1
+      @check_in = Date.new(2017,9,9)
+      @check_out = Date.new(2017,9,12)
+    end
+
+    it "Can be called" do
+      @test_ob.must_respond_to :available_rooms
+    end
+
+    it "Returns an Array of Integers" do
+      @test_ob.available_rooms(@check_in, @check_out).must_be_instance_of Array
+
+      @test_ob.available_rooms(@check_in, @check_out)[0].must_be_instance_of Integer
+
+      @test_ob.available_rooms(@check_in, @check_out).length.must_equal 20
+
+      @test_ob.make_reservation(@room, @check_in, @check_out)
+      @test_ob.available_rooms(@check_in, @check_out).length.must_equal 19
+
+    end
+
+  end
+
+  describe "room_available? method" do
+    before do
+      @room = 1
+      @check_in = Date.new(2017,9,9)
+      @check_out = Date.new(2017,9,12)
+    end
+
+    #https://stackoverflow.com/questions/16599256/testing-private-method-in-ruby-rspec
+    it "Returns true if room is available for requested dates" do
+      @test_ob.send(:room_available?, @room, @check_in, @check_out).must_equal true
+    end
+
+    it "Returns false if room is not available for requested dates" do
+
+      @test_ob.make_reservation(@room, @check_in, @check_out)
+
+      @test_ob.send(:room_available?, @room, @check_in, @check_out).must_equal false
+    end
   end
 
 end
