1491b9149b4deaa8277c7be00303291abf770945
Gale Harrington <gale.harrington19@gmail.com>
2017-09-06 14:26:31 -0700

    Add to reservation_spec.rb

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -3,6 +3,10 @@ module Hotel_System
   class Reservation
     attr_accessor :check_in, :check_out, :room
     def initialize(check_in, check_out)
+      raise ArgumentError.new("Must pass Date object as check_in day and check_out day") unless check_in.class == Date && check_out.class == Date
+      raise ArgumentError.new("Cannot check in and out on same day") if check_in == check_out
+      raise ArgumentError.new("Time Warp") if (check_out <=> check_in) == -1
+
       @check_in = check_in
       @check_out = check_out
       @room = nil
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -2,13 +2,20 @@ require_relative 'spec_helper'
 
 describe 'Reservation' do
   before do
-    check_in = Date.new(2017,9,5)
-    check_out = Date.new(2017,9,7)
-    @my_reservation = Hotel_System::Reservation.new(check_in, check_out)
+    @check_in = Date.new(2017,9,5)
+    @check_out = Date.new(2017,9,7)
+    @my_reservation = Hotel_System::Reservation.new(@check_in, @check_out)
   end
   describe '#initialize' do
-    it "takes a name, a check_in date, and a check_out date as parameters" do
+    it "creates a new instance of Reservation" do
       @my_reservation.must_be_instance_of Hotel_System::Reservation
+    end
+    it "raises an argument error unless passed Date objects" do
+      proc {Hotel_System::Reservation.new("today", "tomorrow")}.must_raise ArgumentError
+    end
+    it "raises an argument error if check_out is same as or before check_in" do
+      proc {Hotel_System::Reservation.new(@check_in, @check_in)}.must_raise ArgumentError
+      proc {Hotel_System::Reservation.new(@check_out, @check_in)}.must_raise ArgumentError
     end
   end
   describe '#cost' do
