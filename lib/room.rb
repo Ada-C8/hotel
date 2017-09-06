d39402366fa1974499ca764d330d5e8d4e5d04a1
Maria McGrew <mcgmar1@outlook.com>
2017-09-06 14:27:31 -0700

    test for checking valid room number method passing and instantiating passing

--- lib/room.rb
+++ lib/room.rb
@@ -1,14 +1,34 @@
+require 'date'
+
 module Hotel
   class Room
 
-    attr_reader :room_num, :status, :dates_reserved
+    RATE = 200
+
+    attr_reader :room_num, :status, :date_reserved
 
 
-    def initialize(room_num, status = :open, dates_reserved)
+    def initialize(room_num, status = :open, date_reserved)
       @room_num = room_num
       @status = status
-      @dates_reserved = dates_reserved
+      @date_reserved = Date.new
     end
+
+    def room_numbers(room_num)
+      if room_num.between?(1, 20)
+        return true
+      else
+        raise ArgumentError.new "Not a valid room number"
+      end
+    end
+
+    def self.all
+      room = Room.new(@room_num, @status, @date_reserved)
+      @all_rooms = []
+      @all_rooms << room
+      return @all_rooms
+    end
+
 
   end #room
 end # module
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,20 +1,46 @@
 require_relative 'spec_helper'
 
 describe "Hotel#Room wave 1" do
-  describe "instantiate room" do
 
-    it "create a new room with a room number" do
-      room_num = 4
-      status = :open
-      dates_reserved = "look at dates"
+  before "room instance" do
+    room_num = 4
+    status = :open
+    date_reserved = Date.new(2018, 01, 01)
 
-      room = Hotel::Room.new(room_num, status, dates_reserved)
-
-      room.must_respond_to :room_num
-      room.room_num.must_equal 4
-      room.room_num.must_be_kind_of Integer
+    @room = Hotel::Room.new(room_num, status, date_reserved)
 
   end
+
+  describe "instantiate room" do
+
+    it "create a new instance of room" do
+      @room.must_respond_to :room_num
+      @room.room_num.must_equal 4
+      @room.room_num.must_be_kind_of Integer
+
+      @room.must_respond_to :status
+      @room.status.must_equal :open
+      @room.status.must_be_kind_of Symbol
+
+      @room.must_respond_to :date_reserved
+      @room.date_reserved.must_equal Date.new
+    end
+  end
+
+  describe "hotel#room" do
+    it "stores all rooms in an Array" do
+      Hotel::Room.all.must_be_kind_of Array
+    end
+
+    it "accepts a vaild room number" do
+      @room.room_numbers(4).must_equal true
+    end
+
+    it "raises an ArgumentError if the room number is not 1-20" do
+      proc{
+        @room.room_numbers(25)
+      }.must_raise ArgumentError
+    end
 
   end
 
