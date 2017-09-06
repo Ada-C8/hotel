876b033b28e0081f9d782432bd0c7a4b641249d7
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-06 11:48:29 -0700

    Adds spec test for valid date method that includes a custom exception

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -4,22 +4,42 @@ describe "DateRange Class" do
   before do
     check_in = Date.new(2017,9,5)
     check_out = Date.new(2017,9,8)
+    # check_out_bad = Date.new(2017,9,2)
     @vacation = Hotel::DateRange.new(check_in,check_out)
+    @vacation_bad_dates = Hotel::DateRange.new(check_out,check_in)
   end
   # will respond to @check_in, @check_out
   #will create a new instance of date_range that will output an array
   #will validate inputs
   describe "initialize" do
-    it "Instance of DateRange class must respond to check_in and check_out" do
+    it "Instance of DateRange class must respond to instance variables (check_in,check_out,nights, nights_arr" do
       @vacation.must_respond_to :check_in
       @vacation.must_respond_to :check_out
+      @vacation.must_respond_to :nights
+      @vacation.must_respond_to :nights_arr
     end
     it "check_in and check_out are of the Date class" do
       @vacation.check_in.must_be_kind_of Date
       @vacation.check_out.must_be_kind_of Date
     end
+    it "nights is an Integer" do
+      @vacation.nights.must_be_kind_of Integer
+    end
+    it "nights returns the correct number equal to the difference of checkout-checkin" do
+      @vacation.nights.must_equal 3
+    end
+    it "nights_arr is an array" do
+      @vacation.nights.must_be_kind_of Array
+    end
+  end
+  describe "valid_date? method" do
+    it "valid_date? will raise an error if check_out if prior to check_in" do
+      proc{Hotel::DateRange.new(check_out,check_in)}.must_raise InvalidDateRangeError
+    end
   end
   describe "make_nights_arr method" do
-
+    # it "" do
+    #   binding.pry
+    # end
   end
 end
