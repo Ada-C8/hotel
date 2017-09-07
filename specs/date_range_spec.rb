e43b6a8281bd42fd55567faa9953ce2d4324dfef
Amy Cash <amymcash@gmail.com>
2017-09-06 21:00:53 -0700

    Create Check_in and Check_out Variables and Date Range Array

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -2,20 +2,36 @@ require 'pry'
 require_relative 'spec_helper'
 
 describe "DateRange Class" do
-  # before do
-  #   @date = Hotel::DateRange.new
-  # end #before
+  before do
+    @date_test = Hotel::DateRange.new
+  end #before
 
   describe "Instantiation" do
     before do
       @test = Hotel::DateRange.new
     end
+
     it "can instantiate a DateRange class" do
       Hotel::DateRange.new.must_be_kind_of Hotel::DateRange
-    end #admin class
+    end #instantiate DateRange
 
     # it "can create a hash of rooms" do
     #   @test_hotel.rooms.must_be_kind_of Hash
     # end #rooms
   end #Instantiation
-end #admin
+
+  describe "Check In, Check Out, and Range Objects" do
+    it "can create a date object for a check in date: " do
+      @date_test.check_in("2001/1/1").must_be_kind_of Date
+    end #check in
+
+    it "can create a date object for a check out date: " do
+      @date_test.check_out("2001/1/10").must_be_kind_of Date
+    end #check_out
+
+    it "can create an array of dates between the check-in and check out" do
+      @date_test.create_range.must_be_kind_of Array
+    end #date_range
+  end #check_in, check_out, range
+
+end #DateRange
