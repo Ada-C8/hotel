125884db045f647726b21b4a098836a017d26ae8
Eva Sawyer <esawyer25@gmail.com>
2017-09-07 10:01:45 -0700

    added a before loop to dry out code

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -1,44 +1,36 @@
 require_relative 'spec_helper'
 #
 describe "My_Hotel::Date_Range" do
+  before do
+    @dec20 = Date.civil(2017, 12, 20)
+    @dec26 = Date.civil(2017,12,26)
+    @holiday_dates = My_Hotel::Date_Range.new(@dec20, @dec26)
+  end
+
   describe "initialize" do
     it "can be created" do
-      first_night = Date.civil(2017, 12, 20)
-      last_night = Date.civil(2017, 12, 23)
-      holiday_dates = My_Hotel::Date_Range.new(first_night, last_night)
-      holiday_dates.must_be_kind_of My_Hotel::Date_Range
+      @holiday_dates.must_be_kind_of My_Hotel::Date_Range
 
-      holiday_dates.nights.must_be_kind_of Range
-      holiday_dates.nights.to_a.length.must_equal 4
+      @holiday_dates.nights.must_be_kind_of Range
+      @holiday_dates.nights.to_a.length.must_equal 4
     end
   end
   #
   describe "overlap?" do
     it "must return true of the date overlaps with the reservation range" do
-      first_night = Date.civil(2017, 12, 20)
-      last_night = Date.civil(2017, 12, 26)
-      dec25 = Date.civil(2017,12,25)
-      dec20 = Date.civil(2017,12,20)
-      holiday_dates = My_Hotel::Date_Range.new(first_night,last_night)
-      holiday_dates.must_be_kind_of My_Hotel::Date_Range
-
-      holiday_dates.overlap?(dec25).must_equal true # nominal case true
-      holiday_dates.overlap?(dec20).must_equal true # checkin day
+        dec25 = Date.civil(2017,12,25)
+      @holiday_dates.overlap?(dec25).must_equal true # nominal case true
+      @holiday_dates.overlap?(@dec20).must_equal true # checkin day
 
     end
 
     it "must return false if date does not overlap with the reservation range" do
-      first_night = Date.civil(2017, 12, 20)
-      last_night = Date.civil(2017, 12, 26)
       dec19 = Date.civil(2017,12,19)
       dec29 = Date.civil(2017,12,29)
       bc = Date.civil(-4,3,1)
-      holiday_dates = My_Hotel::Date_Range.new(first_night,last_night)
-      holiday_dates.must_be_kind_of My_Hotel::Date_Range
-
-      holiday_dates.overlap?(dec19).must_equal false # nominal case false
-      holiday_dates.overlap?(dec29).must_equal false # checkout day
-      holiday_dates.overlap?(bc).must_equal false # a date a long time ago
+      @holiday_dates.overlap?(dec19).must_equal false # nominal case false
+      @holiday_dates.overlap?(dec29).must_equal false # checkout day
+      @holiday_dates.overlap?(bc).must_equal false # a date a long time ago
     end
 
   end
--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -1,154 +1,113 @@
 require_relative 'spec_helper'
 
 describe "My_Hotel::Hotel" do
+  before do
+    @ritz = My_Hotel::Hotel.new
+    @feb1 = Date.civil(2017,2,1)
+    @feb3 = Date.civil(2017,2,3)
+    @feb5 = Date.civil(2017,2,5)
+    @feb6 = Date.civil(2017,2,6)
+    @apr3 = Date.civil(2017,4,3)
+    @apr6 = Date.civil(2017,4,6)
+    @may6 = Date.civil(2017,5,6)
+  end
+
   describe "initialize" do
     it "creates an instance of Hotel" do
-      ritz = My_Hotel::Hotel.new
-      ritz.must_be_kind_of My_Hotel::Hotel
-      ritz.rooms.must_be_kind_of Hash
-      ritz.rooms.length.must_equal 20
+      @ritz.must_be_kind_of My_Hotel::Hotel
+      @ritz.rooms.must_be_kind_of Hash
+      @ritz.rooms.length.must_equal 20
 
     end
   end
 
   describe "make_reservation" do
     it "can make a reservation" do
-      hotel_california = My_Hotel::Hotel.new
-      hotel_california.must_be_kind_of My_Hotel::Hotel
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,5)
-
-      holiday = hotel_california.make_reservation(first_night, last_night)
+      @ritz.must_be_kind_of My_Hotel::Hotel
+      holiday = @ritz.make_reservation(@feb1, @feb5)
       holiday.must_be_kind_of My_Hotel::Reservation
       #should reject bad dates
     end
 
     it "calculates the cost" do
-      hotel_california = My_Hotel::Hotel.new
-      hotel_california.must_be_kind_of My_Hotel::Hotel
-
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,5)
-      holiday = hotel_california.make_reservation(first_night, last_night)
+      holiday = @ritz.make_reservation(@feb1, @feb5)
       holiday.cost.must_equal 1000 #nominal case
     end
 
     it "assigns a room number" do
-      hotel_california = My_Hotel::Hotel.new
-      hotel_california.must_be_kind_of My_Hotel::Hotel
-
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,2)
-      holiday = hotel_california.make_reservation(first_night, last_night)
+      holiday = @ritz.make_reservation(@feb1, @feb5)
       holiday.room_number.must_be_kind_of Integer
       (1..20).must_include holiday.room_number
     end
 
     it "updates the list_of_reservations" do
-      hotel_california = My_Hotel::Hotel.new
-      hotel_california.must_be_kind_of My_Hotel::Hotel
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,2)
-      holiday = hotel_california.make_reservation(first_night, last_night)
-      hotel_california.all_reservations.must_be_kind_of Array
-      hotel_california.all_reservations.length.must_equal 1
-
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,3)
-      holiday = hotel_california.make_reservation(first_night, last_night)
-      hotel_california.all_reservations.length.must_equal 2
+      @ritz.make_reservation(@feb1, @feb5)
+      @ritz.all_reservations.must_be_kind_of Array
+      @ritz.all_reservations.length.must_equal 1
+      @ritz.make_reservation(@feb1, @feb3)
+      @ritz.all_reservations.length.must_equal 2
     end
   end
   #
   describe "find_reservations_by_date" do
     it "given a date, it returns an array of reservations on that date" do
-      hotel_california= My_Hotel::Hotel.new
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,5)
-      hotel_california.make_reservation(first_night, last_night)
-      first_night = Date.civil(2017,2,3)
-      last_night = Date.civil(2017,5,6)
-      hotel_california.make_reservation(first_night,last_night)
-      first_night = Date.civil(2017,4,3)
-      last_night = Date.civil(2017,4,6)
-      hotel_california.make_reservation(first_night, last_night)
+      @ritz.make_reservation(@feb1, @feb5)
+      @ritz.make_reservation(@feb3, @may6)
+      @ritz.make_reservation(@apr3, @apr6)
+      feb3_reservations = @ritz.find_reservations_by_date(@feb3)
 
-      check_date = Date.civil(2017,2,4)
-      feb4 = hotel_california.find_reservations_by_date(check_date)
-
-      feb4.must_be_kind_of Array
-      feb4.length.must_equal 2
-
-      check_date = Date.civil(2017,2,6)
-      feb5 = hotel_california.find_reservations_by_date(check_date)
-      feb5.length.must_equal 1
+      feb3_reservations.must_be_kind_of Array
+      feb3_reservations.length.must_equal 2
+      feb6_reservations = @ritz.find_reservations_by_date(@feb6)
+      feb6_reservations.length.must_equal 1
     end
 
     it "given a date, it returns an empty array if there are no reservations on that date" do
-      check_date = Date.civil(2017,5,21)
-      first_night = Date.civil(2017,4,3)
-      last_night = Date.civil(2017,4,6)
-      hotel_california= My_Hotel::Hotel.new
-      hotel_california.make_reservation(first_night, last_night)
-      may21 = hotel_california.find_reservations_by_date(check_date)
-      may21.must_equal []
+      @ritz.make_reservation(@feb1, @feb5)
+      may6_reservations = @ritz.find_reservations_by_date(@may6)
+      may6_reservations.must_equal []
     end
   end
 
   describe "find_by_reservation_id" do
     it "given a reservation id, it returns that reservation" do
-      hotel_california= My_Hotel::Hotel.new
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,5)
-      a = hotel_california.make_reservation(first_night, last_night)
-      first_night = Date.civil(2017,2,3)
-      last_night = Date.civil(2017,5,6)
-      b = hotel_california.make_reservation(first_night,last_night)
-
-      hotel_california.find_by_reservation_id(1).must_equal a
-      hotel_california.find_by_reservation_id(2).must_equal b
+      first = @ritz.make_reservation(@feb1, @feb5)
+      second = @ritz.make_reservation(@feb3,@may6)
+      @ritz.find_by_reservation_id(1).must_equal first
+      @ritz.find_by_reservation_id(2).must_equal second
     end
 
     it "given a reservation id, it returns nil if that reservation does not exist" do
-      hotel_california= My_Hotel::Hotel.new
-      hotel_california.find_by_reservation_id(5).must_be_nil
+      @ritz.find_by_reservation_id(5).must_be_nil
     end
   end
 
   describe "find unreserved rooms by date" do
     it "returns an hash" do
-      hotel_california= My_Hotel::Hotel.new
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,5)
-      nights = (first_night .. last_night)
-      free_rooms = hotel_california.find_unreserved_rooms(nights)
-
+      nights = (@feb1 .. @feb5)
+      free_rooms = @ritz.find_unreserved_rooms(nights)
       free_rooms.must_be_kind_of Hash
       free_rooms.length.must_equal 20
     end
 
     it "returns a hash of the available rooms" do
-      hotel_california= My_Hotel::Hotel.new
-      first_night = Date.civil(2017,2,1)
-      last_night = Date.civil(2017,2,5)
-      nights = (first_night .. last_night)
-
+      nights = (@feb1 .. @feb5)
       index = 1
       20.times do
-        hotel_california.make_reservation(first_night,last_night)
-        free_rooms = hotel_california.find_unreserved_rooms(nights)
+        @ritz.make_reservation(@feb1, @feb5)
+        free_rooms = @ritz.find_unreserved_rooms(nights)
         free_rooms.length.must_equal 20-index
         index += 1
       end
-      hotel_california.make_reservation(first_night,last_night)
-      free_rooms = hotel_california.find_unreserved_rooms(nights)
+      @ritz.make_reservation(@feb1, @feb5)
+      free_rooms = @ritz.find_unreserved_rooms(nights)
       free_rooms.must_equal nil
     end
   end
   #
-# I think I should split up find_unreserved rooms and
-#reservation so that first the user can see if there are
-#any unreserved rooms for that night.  If so, they can assign one of them by passing it as an argument into make_reservation
+  # I think I should split up find_unreserved rooms and
+  #reservation so that first the user can see if there are
+  #any unreserved rooms for that night.  If so, they can assign one of them by passing it as an argument into make_reservation
   #
   #
   # end
