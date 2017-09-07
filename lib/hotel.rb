9d73ba141b45af5d8ca1d0367eb2c6aea2ea4bb5
VirtualLindsey <tapircode@gmail.com>
2017-09-06 23:24:20 -0700

    forgot to save hotel

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -16,55 +16,6 @@ class Hotel
 	
   end
   
-  def reserveRoom(room_number, startDate, endDate)
-	current_reservations = @reservations_by_room[room_number]
-	
-	current_reservations.each do |i|
-		if startDate < i.startDate  
-			#end date happens after the start of an existing reservation
-			if (endDate - i.startDate).to_i > 0 
-				raise ArgumentError, "This room is booked during the dates requested"
-			end
-			#if the start dates are the same, or the start date is less than the end date
-		elsif startDate == i.startDate || (startDate - i.endDate).to_i < 0 
-			raise ArgumentError, "This room is booked during the dates requested"
-		end
-	end
-	new_reservation = Reservation.new(room_number, startDate, endDate)
-	@reservations_by_room[room_number] << new_reservation
-	
-	if @reservations_by_date.key?(startDate)
-		@reservations_by_date[startDate] << new_reservation
-	else
-		@reservations_by_date[startDate] = [new_reservation]
-	end
-		
-  end
-  
-  def getAllRooms
-	return @rooms
-  end
-  
-  def getOpenRooms(startDate, endDate)
-	available_rooms = []
-	@rooms.length.times do |i|
-		available_rooms[i] = true
-	end
-	
-	current_date = startDate
-	while current_date < endDate do
-		if @reservations_by_date.key?(current_date)
-			current_reservations = @reservations_by_date[current_date]
-			current_reservations.each do  |this_room|
-				available_rooms[this_room.room_number] = false
-			end
-		end
-		current_date += 1
-	end
-	
-	for 
-	return available_roooms
-  end
   
   
 end
