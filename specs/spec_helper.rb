a66fab848c0fa510665aa6616e87d493a84a101c
nkiru onwuneme <generalhrh@gmail.com>
2017-09-07 10:12:13 -0700

    Added room and booking_system to spec file

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -10,6 +10,9 @@ SimpleCov.start
 
 #require any classes
 require_relative '../lib/date_range'
+require_relative '../lib/reservation'
+require_relative '../lib/room'
+require_relative '../lib/booking_system'
 
 
 
