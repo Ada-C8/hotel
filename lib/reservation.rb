3414dbc5fbc03abd36627541e8712a90071b7082
Jan Edrozo <jedrozo@gmail.com>
2017-09-07 10:20:55 -0700

    Reservation: get_total_cost and initialize arguments count

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -5,7 +5,7 @@ module BookingSystem
   class Reservation
     attr_reader :first_name, :last_name, :room_id, :room_rate, :reserv_id, :start_date, :end_date, :room_count, :total_cost
 
-    def initialize(first_name, last_name, room_id, room_rate, reserv_id, start_date, end_date, room_count, total_cost) #assuming dates are already entered as objects using Date.parse("DD/MM/YYYY")
+    def initialize(first_name, last_name, room_id, room_rate, reserv_id, start_date, end_date, room_count) #assuming dates are already entered as objects using Date.parse("DD/MM/YYYY")
       # raise ArgumentError.new("start_date must be a string in the format: DD/MM/YYYY") if !start_date
       @first_name = first_name
       @last_name = last_name
@@ -15,38 +15,15 @@ module BookingSystem
       @room_id = room_id
       @room_rate = room_rate
       @room_count = room_count
-      @total_cost = total_cost
+
+      @total_cost = 0
     end
 
-    def get_total_cost
-      date_diff = end_date - start_date
-      total_cost = date_diff * room_rate
-      return total_cost
+    def get_total_cost #(start_date, end_date, room_rate, room_count) would hotel use these as arguments to get total_cost?
+      date_diff = @end_date - @start_date
+      @total_cost = date_diff * (@room_rate * @room_count)
+      return @total_cost
     end
 
   end#of_Reservation_class
 end#of_module_BookingSystem
-
-# module BookingSystem
-#   class Reservation
-#     attr_reader :first_name, :last_name, :reserv_id, :start_date, :end_date, :room_id, :total_cost
-#
-#     def initialize(first_name, last_name, reserv_id, start_date, end_date, room_id, total_cost) #assuming dates are already entered as objects using Date.parse("DD/MM/YYYY")
-#       # raise ArgumentError.new("start_date must be a string in the format: DD/MM/YYYY") if !start_date
-#       @first_name = first_name
-#       @last_name = last_name
-#       @reserv_id = reserv_id #how am I going to generate a reserv_id?
-#       @start_date = start_date
-#       @end_date = end_date
-#       @room_id = room_id #Booking::Room.id
-#       @total_cost = total_cost
-#     end
-#
-#     def get_total_cost
-#       date_diff = end_date - start_date
-#
-#       date_diff *
-#     end
-#
-#   end#of_Reservation_class
-# end#of_module_BookingSystem
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -12,8 +12,8 @@ describe "Reservation" do
     start_date = Date.parse("1/9/2017")
     end_date = Date.parse("3/9/2017")
     room_count = 1
-    total_cost = 400.00
-    @reservation_test = BookingSystem::Reservation.new(first_name, last_name, room_id, room_rate, reserv_id, start_date, end_date, room_count, total_cost)
+
+    @reservation_test = BookingSystem::Reservation.new(first_name, last_name, room_id, room_rate, reserv_id, start_date, end_date, room_count)
   end
 
   describe "#initialize" do
@@ -38,15 +38,12 @@ describe "Reservation" do
       @reservation_test.room_rate.must_be_instance_of Float
     end
 
-    it "can take in reservation info as arguments: reserv_id, room_count, total_cost" do
+    it "can take in reservation info as arguments: reserv_id, room_count" do
       @reservation_test.reserv_id.must_equal 1221
       @reservation_test.reserv_id.must_be_instance_of Integer #arbitrarily deciding the id is an integer MAY NOT BE NECESSARY to include
 
       @reservation_test.room_count.must_equal 1
       @reservation_test.room_count.must_be_instance_of Integer
-
-      @reservation_test.total_cost.must_equal 400.00
-      @reservation_test.total_cost.must_be_instance_of Float
     end
 
     it "can take in reservation date objects as arguments: start_date, end_date" do
@@ -59,10 +56,15 @@ describe "Reservation" do
 
   end
 
-  # describe "#get_total_cost" do
-  #   it "can calculate a total " do
-  #
-  #   end
-  # end
+  describe "#get_total_cost" do
+    it "can calculate a total" do
+
+      @reservation_test.get_total_cost.must_equal 400.00
+      @reservation_test.get_total_cost.must_be_instance_of Float
+
+      @reservation_test.total_cost.must_equal 400.00
+      @reservation_test.total_cost.must_be_instance_of Float
+    end
+  end
 
 end#of_"Reservation"
