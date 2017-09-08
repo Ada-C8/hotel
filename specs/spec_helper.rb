b56ae49c9d2ddbd8a34c2f8dc758fc1e331272b8
Amy Lee <amy.yj.lee@gmail.com>
2017-09-07 23:50:14 -0700

    Added Block to required files

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -7,5 +7,6 @@ require_relative '../lib/Booking'
 require_relative '../lib/Reservation'
 require_relative '../lib/Room'
 require_relative '../lib/Guest'
+require_relative '../lib/Block'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
