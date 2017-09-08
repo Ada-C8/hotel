3c1dda5fd70ab420c9eb15447bab8baaaf5b8e0e
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-09-08 13:21:09 -0700

    Simplify self.available method in Block

--- lib/Block.rb
+++ lib/Block.rb
@@ -10,18 +10,17 @@ module Hotel
     @@blocks = []
 
     def initialize(start_date, end_date, number_of_rooms, discount = 0.1)
-      raise ArgumentError.new("Blocks can have between 1 and 5 rooms") if (1..5).to_a.include?number_of_rooms == false
+      raise ArgumentError if (1..5).to_a.include?number_of_rooms == false
       @block_id = Block.all.length + 1
       @start_date = start_date
       @end_date = end_date
       @discount = discount
       @rooms = sample_available_rooms(start_date, end_date, number_of_rooms)
-      raise NoRoomsAvailableError if rooms.length < number_of_rooms
       collect_instance
     end
 
     def self.rooms_left(block_id)
-      block = Block.all.select { |a_block| a_block.block_id == block_id }[0]
+      block = Block.all.find { |a_block| a_block.block_id == block_id }
       return Reservation.sample_available_rooms(block.start_date, block.end_date, block_id, block.rooms.length)
     end
 
@@ -40,18 +39,17 @@ module Hotel
       blocked_room_numbers = room_numbers - Hotel::Block.available(start_date, end_date)
       reserved_room_numbers = room_numbers - Hotel::Reservation.available(start_date, end_date)
       available_room_numbers = room_numbers - blocked_room_numbers - reserved_room_numbers
+      raise NoRoomsAvailableError if available_room_numbers.length < number_of_rooms
       return available_room_numbers.sample(number_of_rooms)
     end
 
     def self.available(start_date, end_date)
-      available_rooms = Room.all.map { |room| room.room_num }
-      overlapping_blocks = Block.all do |block|
-        self.overlapping?(start_date, end_date, block.start_date, block.end_date) == true
+      all_rooms = Room.all.map { |room| room.room_num }
+      overlapping_rooms = []
+      Block.all.each do |block|
+        overlapping_rooms += block.rooms if self.overlapping?(start_date, end_date, block.start_date, block.end_date) == true
       end
-      overlapping_blocks.each do |block|
-        available_rooms -= block.rooms
-      end
-      return available_rooms
+      return all_rooms - overlapping_rooms
     end
 
   end # Block class
