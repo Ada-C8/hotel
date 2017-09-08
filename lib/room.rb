4b73c4014485a80cb3bc661a00b297d776d17431
Sara Frandsen <sara.frandsen@gmail.com>
2017-09-08 11:47:32 -0700

    @@rooms_available, diff room for diff guests

--- lib/room.rb
+++ lib/room.rb
@@ -2,20 +2,23 @@ require 'date'
 
 module Hotel
   class Room
+      @@rooms_available = (1..20).to_a
+    end
+
     attr_reader :room_number, :rooms_available
+    def self.rooms_available
     def initialize
-      @rooms_available = (1..20).to_a
       @room_number = room_number
     end # end #initialize
 
     def assign_room
-      @room_number = @rooms_available.shift # removes first element of array
+      @room_number = @@rooms_available.shift # removes first element of array
 
         # go through array and remove the first room available, (shorten the number of rooms available by shortening the array) then shovel back in as guests check out
     end # end #assign_room
 
     def check_out_of_room
-      @rooms_available << @room_number
+      @@rooms_available << @room_number
     end # end #check_out_of_room
 
     def is_available?(checkin, check_out)
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -13,20 +13,22 @@ describe 'Room' do
 
   describe 'assign_room' do
     it 'removes unavailable rooms from the rooms_available array each time a room is booked' do
-      @puppy_room.rooms_available.length
-      @puppy_room.assign_room
-      @puppy_room.rooms_available.length.must_equal 19
+      Hotel::Room.rooms_available.length # 20
+      @puppy_room.assign_room # 1
+      Hotel::Room.rooms_available.length.must_equal 19
     end # end test
 
-    it 'assigns room number and not index number' do
-      @puppy_room.assign_room
-      @puppy_room.assign_room
-      @puppy_room.assign_room.must_equal 3
+    it 'assigns a different room number as they are assigned and made unavailable' do
+      @puppy_room.assign_room # 1
+      @kitten_room = Hotel::Room.new
+      @kitten_room.assign_room # 2
+      @bat_room = Hotel::Room.new
+      @bat_room.assign_room.must_equal 3
 
       ####################################
       # okay so in this test, i want it to change the room number as they are booked. so #assign_room is called 3 times, room number should be 3. BUT, what is happening, is it's the SAME GUEST booking 3 rooms. if i make a new instance of Hotel::Room, obv it'll just reset the array and the room assigned would be 1. how do????)
       #####################################
-      
+
     end # end test
   end # end assign_room
 end # end all Room tests
