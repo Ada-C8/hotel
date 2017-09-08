00fb641c182f1d732c0a2c2420bd2f1be1e20445
Kayla Ecker <The Email Associated with Your Github Account>
2017-09-08 11:28:43 -0700

    Forgot to commit before messing everything up - oops.

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,22 +1,57 @@
-module Hotel
+#module Hotel
 
-  class Reservation
-    #dates, check in check out, cost, id
+#class Reservation
+#dates, check in check out, cost, id
 # As an administrator, I can access the list of all of the rooms in the hotel
 # As an administrator, I can reserve a room for a given date range
 # As an administrator, I can access the list of reservations for a specific date
 # As an administrator, I can get the total cost for a given reservation
+#work
+require 'date'
 
+module Hotel
 
-  end
+  class Reservation
+    attr_reader :room_number, :start_date, :end_date, :cost
+
+		def initialize(room_number, date, cost=200)
+			@room_number = room_number
+			@start_date = date.start_date
+			@end_date = date.end_date
+
+      #put in something for a cost discount? if nill blah blah blah
+
+			if @start_date == @date.end_date
+				@cost = cost
+			else
+				@cost = cost * (@end_date - @start_date).to_i
+			end
+		end
+
+    #date range business
+
+    if @start_date > @end_date
+      raise ArgumentError, "Start date cannot come after end date!"
+    end
+
+    def self.overlapping?(start_date, end_date, comparison_start_date, comparison_end_date)
+      # start date is within comparison date range
+      return true if start_date >= comparison_start_date && start_date < comparison_end_date
+      # end date is within comparison date range
+      return true if end_date >= comparison_start_date && end_date <= comparison_end_date
+      return false
+    end
+  end #class reservation
+
 
   class Hotel_Rooms
-    attr_reader :number, :id, :rate
+    attr_reader :number, :id, :rate, :room_number
 
     def initialize(number)
       @number = number
-      @id = id 
+      @id = id
       @rate = rate
+      @room_number = room_number
     end
 
     def self.all
@@ -34,12 +69,11 @@ module Hotel
     #error for invalid date range
 
 
+  end #end of class Hotel_Rooms
 
 
 
 
 
 
-
-  end #end of class
-end #end of module
+end #end of module hotel
--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -1,14 +1,24 @@
 require_relative 'spec_helper'
+require_relative '../lib/hotel'
 
-describe "Rooms wave 1" do
-  before do
-    @rooms = Hotel::Hotel_Rooms.new(1)
+# describe "Rooms" do
+#   before do
+#     @rooms = Hotel::Hotel_Rooms.new(1)
+#   end
+#
+#   it "can initialize rooms" do
+#     @rooms.must_be_instance_of Hotel::Hotel_Rooms
+#     @rooms.number.must_be_kind_of Integer
+#     #@rooms.room_id.must_equal 1
+#   end
+
+  describe "Room" do
+    describe "initialize" do
+      it "checks initialize" do
+        room = Hotel::Hotel_Rooms.new(1)
+        room.must_respond_to :room_number
+  	  room.room_number.must_be_kind_of Integer
+  	  room.room_number.must_equal 1
+      end
+    end
   end
-
-  it "can initialize rooms" do
-    @rooms.must_be_instance_of Hotel::Hotel_Rooms
-    @rooms.number.must_be_kind_of Integer
-  end
-
-
-end
