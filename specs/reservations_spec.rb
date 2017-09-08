1ebef790dbe4a5b56c94027c1ad67aad88d3172c
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-08 12:26:17 -0700

    Attempt to DRY up reservations spec tests

--- specs/reservations_spec.rb
+++ specs/reservations_spec.rb
@@ -6,8 +6,6 @@ describe "Reservations class" do
     @hotel = Hotel::Reservations.new
     @hotel.make_reservation(@check_in,@check_out,1)
     @hotel.make_reservation(@check_in,@check_out,1)
-    @hotel.make_reservation(@check_in,@check_out + 2,1)
-    @hotel.make_reservation(@check_in + 1,@check_out + 1,4)
   end
   describe "initializes" do
     it "initializes" do
@@ -33,13 +31,13 @@ describe "Reservations class" do
       holder.must_be_kind_of Array
     end
     it "return a list of rooms booked for a given date range, rooms should be unique" do
-      arr_booked = @hotel.check_reservations(@check_in,@check_out + 3)
+      arr_booked = @hotel.check_reservations(@check_in,@check_out)
       arr_booked_unique = arr_booked.uniq
-      arr_booked.length.must_equal 5
+      arr_booked.length.must_equal 2
       arr_booked.length.must_equal arr_booked_unique.length
     end
     it "returns an empty array if nothing is booked" do
-      @hotel.check_reservations(@check_in + 5,@check_out + 6).must_be_empty
+      @hotel.check_reservations(@check_in + 1,@check_out + + 1).must_be_empty
     end
   end
   describe "check_availability" do
@@ -47,25 +45,23 @@ describe "Reservations class" do
       @hotel.check_availability(@check_in,@check_out).must_be_kind_of Array
       @hotel.check_availability(@check_in,@check_out)[0].must_be_kind_of Hotel::Room
     end
-    it "returns a list of rooms available, rooms should be unique" do
+    it "returns a list of the correct number of rooms available, rooms should be unique" do
       rooms_available = @hotel.check_availability(@check_in,@check_out)
+      rooms_available.length.must_equal 18
       rooms_available_unique = rooms_available.uniq
-      rooms_available.length.must_equal rooms_available_unique.length
+      rooms_available.length.must_equal  rooms_available_unique.length
     end
     it "returns an empty array if everything is booked" do
-      @hotel.make_reservation(@check_in - 1,@check_out - 1,20)
-      # binding.pry
-      #will fail until we do make booking method
-      @hotel.check_availability(@check_in - 1,@check_out - 1).must_be_empty
+      @hotel.make_reservation(@check_in,@check_out,18)
+      @hotel.check_availability(@check_in,@check_out).must_be_empty
     end
   end
   describe "make_reservation" do
     it "it will make create object of booking class" do
-      reservation = @hotel.make_reservation(@check_in,@check_out,1)
-      reservation.must_be_kind_of Hotel::Booking
+     @hotel.make_reservation(@check_in,@check_out,1).must_be_kind_of Hotel::Booking
     end
     it "will raise an ArgumentError if no rooms are avabilable" do
-      proc{@hotel.make_reservation(@check_in,@check_out,21)}.must_raise InvalidRoomQuantity
+      proc{@hotel.make_reservation(@check_in,@check_out,19)}.must_raise InvalidRoomQuantity
     end
     it "it adds a new reservation to all_reservations" do
       before = @hotel.all_reservations.length
@@ -76,17 +72,21 @@ describe "Reservations class" do
     it "make sure it creates consecutive ID numbers" do
       id1 = @hotel.make_reservation(@check_in,@check_out,1).id
       id2 = @hotel.make_reservation(@check_in,@check_out,1).id
-      binding.pry
       id2.must_equal (id1 + 1)
     end
-    xit "picks the next consecutive room number available" do
-      #not sure how to to test this
-    end
-    xit "you can book a room on the check out date" do
-      # not sure about this...
+    it "picks the next consecutive room number available" do
+      room_num1 = @hotel.all_reservations[0].rooms[0].room_number
+      room_num2 = @hotel.all_reservations[1].rooms[0].room_number
+      room_num2.must_equal (room_num1 + 1)
     end
     it "raise ArgumentError if you request more than 20 rooms" do
       proc{@hotel.make_reservation(@check_in-3,@check_out-3,21)}.must_raise InvalidRoomQuantity
+    end
+    it "lets you book on the check out date" do
+      # books all room on a given date
+      @hotel.make_reservation(@check_in + 1, @check_out + 1,20)
+      # can book rooms on the following day AKA checkout day
+      @hotel.make_reservation(@check_in + 2, @check_out + 2,20)
     end
   end
 end
