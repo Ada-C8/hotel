0532edf42b666f762012681ab8bbe2bd68c6d61e
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-09-08 10:07:29 -0700

    refactored #range_to and #range_with to be more DRY

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,27 +1,11 @@
 module DateRange
   require 'date'
   def self.range_to(start_date, end_date)
-    start_date = validate(start_date)
-    end_date = validate(end_date)
-    raise(ArgumentError, 'Start date must be before end date') unless start_date < end_date
-    dates = []
-    while start_date < end_date
-      dates << start_date
-      start_date += 1
-    end
-    dates
+    find_range(start_date, end_date)
   end
 
   def self.range_with(start_date, end_date)
-    start_date = validate(start_date)
-    end_date = validate(end_date)
-    raise(ArgumentError, 'Start date must be before end date') unless start_date < end_date
-    dates = []
-    while start_date <= end_date
-      dates << start_date
-      start_date += 1
-    end
-    dates
+    find_range(start_date, end_date, :inclusive)
   end
 
   def self.overlap?(first_start, first_end, second_start, second_end)
@@ -50,5 +34,22 @@ module DateRange
     rescue
       raise(TypeError, "Invalid input #{input.class}-must be Date or String")
     end
+  end
+
+  def self.invalid_dates_error
+    raise(InvalidDatesError, 'Start date must be before end date')
+  end
+
+  def self.find_range(start_date, end_date, flag = false)
+    start_date = validate(start_date)
+    end_date = validate(end_date)
+    invalid_dates_error unless start_date < end_date
+    dates = []
+    while start_date < end_date
+      dates << start_date
+      start_date += 1
+    end
+    dates << end_date if flag == :inclusive
+    dates
   end
 end
