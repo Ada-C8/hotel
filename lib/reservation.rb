4b8a9fd301d6c15277c37e01883abdf8c28b1476
Gale Harrington <gale.harrington19@gmail.com>
2017-09-08 13:03:55 -0700

    Use let() in reservation_spec

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -3,7 +3,7 @@ module Hotel_System
   class Reservation
     attr_accessor :check_in, :check_out, :room
     def initialize(check_in, check_out)
-      raise ArgumentError.new("Must pass Date object as check_in day and check_out day") unless check_in.class == Date && check_out.class == Date
+      raise ArgumentError.new("Check_in Date object, check_out Date object") unless check_in.class == Date && check_out.class == Date
       raise ArgumentError.new("Cannot check in and out on same day") if check_in == check_out
       raise ArgumentError.new("Time Warp") if (check_out <=> check_in) == -1
 
