75eb54dcbe6de00f37e89ae2b551f73a035accfc
Maria McGrew <mcgmar1@outlook.com>
2017-09-08 10:44:50 -0700

    created total_nights method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -8,6 +8,11 @@ module Administration
       @check_out = check_out
     end
 
+    def total_nights
+      total_nights = (@check_out - @check_in)
+      return total_nights.ceil
+    end
+
 
   end #reservation
 end #module
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -4,14 +4,25 @@ describe "Administration#Reservation" do
 
   describe "instantiate a reservation" do
     it "has a check_in and check out date" do
-      check_in = Date.new(2017, 8, 30)
+      check_in = Date.new(2017, 9, 1)
       check_out = Date.new(2017, 9, 5)
       reservation = Administration::Reservation.new(check_in, check_out)
 
       reservation.must_respond_to :check_in
       reservation.check_in.must_equal check_in
       reservation.check_in.must_be_kind_of Date
-end
+    end
+
+  describe "#total_nights" do
+    it "returns the total number of nights for a date range" do
+      check_in = Date.new(2017, 9, 1)
+      check_out = Date.new(2017, 9, 5)
+      reservation = Administration::Reservation.new(check_in, check_out)
+
+      reservation.total_nights.must_equal 4
+
+    end
+  end
 
 
 
