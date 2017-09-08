6eb283925b754f5bf0c08831aff50c773855a4b8
Tanisha <tanishahampden@gmail.com>
2017-09-07 18:03:13 -0700

    add room_id variable

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,9 +1,10 @@
 module Hotel
   class Reservation
     attr_reader :id, :date_range
-    def initialize(id,check_in,check_out)
+    def initialize(id,room_id,check_in,check_out)
       @id = id
       @date_range = Hotel::DateRange.new(check_in,check_out)
+      @room_id = room_id
     end
 
     def get_total
