560ab87644bed7b251f2aa398a3701eb9e1469b4
VirtualLindsey <tapircode@gmail.com>
2017-09-07 23:04:21 -0700

    adding tests for room

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,11 +1,13 @@
 require_relative 'spec_helper'
-require_relative '../lib/hotel'
+require_relative '../lib/room'
 
-describe "Hotel" do
+describe "Room" do
   describe "initialize" do
     it "checks initialize" do
-      a = Hotel.new()
-      a.test.must_equal []
+      room = Hotels::Room.new(1)
+      room.must_respond_to :room_number
+	  room.room_number.must_be_kind_of Integer
+	  room.room_number.must_equal 1
     end 
   end
 end
