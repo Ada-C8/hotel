8c999db164c5bd75dfc7a24b0fc16a9878f8087a
nkiru onwuneme <generalhrh@gmail.com>
2017-09-08 11:40:48 -0700

    Added another test for the all method

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -9,6 +9,7 @@ describe "Room" do
     end
   end
 
+# probably can use let to trim code
   describe "self.all" do
     it "returns an array of all rooms" do
       rooms = Hotel::Room.all
@@ -18,6 +19,19 @@ describe "Room" do
     it "checks the number of rooms is correct" do
       rooms = Hotel::Room.all
       rooms.length.must_equal 20
+    end
+
+    # it "checks the first, tenth, and twentienth rooms can be retrieved" do
+    #   rooms = Hotel::Room.all
+    #   puts rooms[0]
+    #   # rooms[0].must_equal Hotel::Room.all
+    # end
+
+    it "checks everything in the array is a room" do
+        rooms = Hotel::Room.all
+        rooms.each do |room|
+          room.must_be_instance_of Hotel::Room
+        end
     end
   end
 end
