9b838838be84e2d1c2834b3d8b7fbded51d263f8
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-09-08 15:53:08 -0700

    add descriptions for exceptions defining their use scenarios

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,11 +1,16 @@
 class NoRoomError < StandardError
+  # Raised when there are not enough available rooms for request
 end
 
 class DatesError < StandardError
+  # Raised when dates are in wrong order, out of range, etc
+  # NOT raised for invalid date input (use ArgumentError)
 end
 
 class NoBlockError < StandardError
+  # Raised when provided block does not exist/cannot be found
 end
 
 class DiscountError < StandardError
+  # Raised when discount is not number from 0-100
 end
