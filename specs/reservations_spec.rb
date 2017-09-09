cb45e426e4380543ae2200fcbaffc06170253fe6
Sara C <srachandler@gmail.com>
2017-09-09 16:45:48 -0700

    tests for reservations.rb updated

--- specs/reservations_spec.rb
+++ specs/reservations_spec.rb
@@ -4,18 +4,25 @@ require 'minitest/skip_dsl'
 require_relative '../lib/reservations'
 require_relative './spec_helper.rb'
 
-# xdescribe "Reservations Wave 1" do
-#
-#   xdescribe "Initialize" do
-#
-#     xit "can initialize a reservation" do
-#       room = 1
-#       dates =
-#     new_res = Hotel::Reservations.new(room, start_date, end_date)
-#     end
-#
-#
-#
-#
-#   end
-# end
+describe "Reservations Wave 1" do
+
+  describe "Initialize" do
+
+    it "can initialize a reservation" do
+      room = Hotels::Rooms.new(1)
+      date_range = [(Date.new(2020, 9, 10)), (Date.new(2020, 9, 11))]
+      new_res = Hotels::Reservations.new(room, date_range)
+
+      new_res.must_be_instance_of Hotels::Reservations
+      new_res.room.must_be_instance_of Hotels::Rooms
+    end
+
+    it "can get total cost of reservation" do
+      room = Hotels::Rooms.new(1)
+      date_range = [(Date.new(2020, 9, 10)), (Date.new(2020, 9, 11))]
+      new_res = Hotels::Reservations.new(room, date_range)
+
+      new_res.calculate_bill.must_equal 400
+    end
+  end
+end
