68a6906f2843bff9a140ff2caa17ffe1cfc0b7ba
hellomarisamorris <hellomarisamorris@gmail.com>
2017-09-10 11:33:56 -0700

    Wave 3: block reservation class set up. inheriting from reservation class. block rate method tests passing

--- lib/block_reservation.rb
+++ lib/block_reservation.rb
@@ -1,1 +1,23 @@
 # block_reservation
+require 'date'
+
+module HotelManagment
+  class BlockReservation < Reservation
+
+    def initialize(first_name, last_name, check_in_date, check_out_date, room_number)
+      super
+      # @first_name = first_name
+      # @last_name = last_name
+      # @check_in_date = check_in_date
+      # @check_out_date = check_out_date
+      # @room_number = room_number
+      # @cost = cost
+    end
+
+    def block_rate
+      nights = @check_out_date - @check_in_date
+      @cost = nights.to_i * Block::BLOCKRATE
+    end
+
+  end #class end
+end #module end
--- specs/block_reservation_spec.rb
+++ specs/block_reservation_spec.rb
@@ -1,1 +1,25 @@
 require_relative 'spec_helper'
+require 'date'
+
+
+describe "Reservaton Class" do
+
+
+
+  describe "block_rate Method" do
+
+    it "Must respond to block_rate" do
+      new_reservation = HotelManagment::BlockReservation.new("marisa", "morris", Date.new(2017,9,5), Date.new(2017,9,10), 1)
+      new_reservation.must_respond_to :block_rate
+    end
+
+
+
+    it "should return the total cost based on the number of nights reserved" do
+
+      new_reservation = HotelManagment::BlockReservation.new("marisa", "morris", Date.new(2017,9,5), Date.new(2017,9,10), 1)
+      new_reservation.block_rate.must_equal 500
+
+    end
+  end
+end
