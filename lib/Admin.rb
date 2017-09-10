bd0473fe70dde8e8304b5f38d9a1511646af856a
Angela Wilson <wilsonange@gmail.com>
2017-09-09 21:51:01 -0700

    Admin Class: Add methods list_of_rooms, add_reservation, list_reservation(date), and list_vacancies. Need to write tests for almost all the listed methods.

--- lib/Admin.rb
+++ lib/Admin.rb
@@ -1,19 +1,74 @@
+require_relative 'reservation'
+
 module Hotel
   class Admin
 
-    attr_reader :room_num
+    attr_reader :room_nums, :reservations
 
     def initialize
-      @reservations = Hotel::Reservation.reservation_array
-      @room_num = []
+
+      @reservations = []
+
+
+      @room_nums = []
       20.times do |i|
-        @room_num << (i + 1)
+        @room_nums << (i + 1)
       end
     end
 
-    # def
-    #
-    # end
+    def list_of_rooms
+      return Array.new(room_nums)
+    end
+
+    def add_reservation(room_selection, check_in, check_out)
+      @reservations << Hotel::Reservation.new(room_selection, check_in, check_out)
+    end
+
+    def list_reservations(date)
+      rez_by_date = []
+
+      @reservations.each do |reservation|
+        if reservation.date_range_array.include?(date)
+          rez_by_date << reservation
+        end
+      end
+
+      return rez_by_date
+    end
+
+
+    def list_vacancies(check_in, check_out)
+      available_rooms = list_of_rooms
+
+      date_range = Hotel::DateRange.new(check_in, check_out).date_range_array
+
+      date_range.each do |date|
+        reservations.each do |reservation|
+          if reservation.date_range_array.include?(date)
+            available_rooms.delete(reservation.room_num)
+          end
+
+        end
+      end
+
+      return available_rooms
+    end
+
 
   end
 end
+
+# puts Hotel::Admin.new.room_selection
+
+# room_num1 = 1
+# check_in = Date.new(2017,2,3)
+# check_out = Date.new(2017, 2,7)
+# # # puts Hotel::DateRange.new(check_in, check_out).date_range_array
+# # reservation = Hotel::Reservation.new(room_num1, check_in, check_out)
+# #
+# # puts "#{reservation.room_nums}"
+#
+#
+# puts Hotel::Admin.new.add_reservation(room_num1, check_in, check_out)
+#
+# puts "This should be the reservations array #{Hotel::Admin.new.reservations}"
--- lib/Reservation.rb
+++ lib/Reservation.rb
@@ -8,34 +8,37 @@ module Hotel
 
     attr_reader :reservation_array, :total_cost, :room_num, :date_range_array
 
-    def initialize( check_in, check_out)
+    def initialize(room_num, check_in, check_out)
 
-      @reservation_array = []
+      # @reservation_array = []
       @total_cost = 0
       @room_num = room_num
-      @date_range_array = DateRange.new(check_in, check_out)
+      @date_range_array = DateRange.new(check_in, check_out).date_range_array
     end
 
 
-    def reservation_id
-      @reservation_array.length + 1
-    end
 
+    # def reservation_id
+    #   Hotel::Admin.reservations.length + 1
+    # end
+
+    
 
     def total_cost
       (@date_range_array.length - 1) * COST_PER_NIGHT
     end
 
-
-
+    # def self.find_all(check_in, check_out)
+    #
+    #   # take array of reservations
+    #   # interate over each to find the reservations for reserved status by date
+    #   @reservation_array.each do |reservation|
+    #     list_reservations =
+    #   end
+    #   return list_reservations
+    # end
+    #
 
 
   end # end class
 end #end module
-
-
-check_in = Date.new(2017,2,3)
-check_out = Date.new(2017, 2,7)
-reservation = Hotel::Reservation.new(check_in, check_out)
-
-puts "#{reservation.reservation_array}"
