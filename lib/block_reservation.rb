5f8c74082dea11af25722e308046a2d9e9c7bde4
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-09 17:12:56 -0700

    Saving state before trying to create mixins because breaking my code would make me cry

--- lib/block_reservation.rb
+++ lib/block_reservation.rb
@@ -35,5 +35,15 @@ module Hotel
       return (cost - (cost * @rate)).to_i
     end
 
+    # TODO: REFACTOR, this is redundant w/ Reservation class
+    # (check_in >= reservation.check_in || check_out < reservation.check_out) && room_num == reservation.room.room_number
+    def overlap?(other_check_in, other_check_out)
+      # return (check_in >= other_check_in || check_out < other_check_out) &&
+      #        room.room_number == other_room_num
+      return (other_check_in == check_in ||
+             (other_check_in < check_in && (other_check_out > check_in && other_check_out < check_out)) ||
+             (other_check_in > check_in && other_check_in < check_out))
+    end
+
   end
 end
