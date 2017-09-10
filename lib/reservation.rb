205a157051ba41e3f622c5d6f08a4a218d9fcfd3
Sara Frandsen <sara.frandsen@gmail.com>
2017-09-09 22:40:33 -0700

    start hash

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -5,7 +5,7 @@ require_relative 'room'
 module Hotel
   class Reservation
     COST = 200 # per night
-    attr_reader :guest_name, :dates, :room, :reservation
+    attr_reader :guest_name, :dates, :room, :reservation :list
 
     def initialize(guest_name, check_in, check_out)
       @guest_name = guest_name
@@ -14,14 +14,11 @@ module Hotel
       @dates = Hotel::DateRange.new(check_in, check_out)
       @room = Hotel::Room.new
       @reservation = reservation
+      @list = list
 
       #reservation: [name: date_range, room, cost]
       #sort_by{|k,v|v}
     end # end initialize
-
-    def reserve_dates
-      @dates.length_of_stay
-    end # end #reserve_dates
 
     def finalize_reservation
         booking = "Name: #{@guest_name}\nCheck In: #{@check_in}\nCheck Out: #{@check_out}\nRoom Number: #{@room.assign_room}\nTotal Cost: $#{(COST * @dates.length_of_stay).to_i}"
@@ -30,7 +27,9 @@ module Hotel
     end # end #finalize_reservation
 
     def list_by_date
+      @list = {
 
+      }
     end # end #list_by_date
   end # end of Reservation
 end # end of Hotel module
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -2,7 +2,11 @@ require_relative 'spec_helper'
 
 describe 'Reservation' do
   before do
+    ############################
+    Hotel::Room.initialize_rooms
+    ############################
     @puppy_expo = Hotel::Reservation.new('Finn', Date.new(2017, 10, 13), Date.new(2017, 10, 31))
+    @kitten_expo = Hotel::Reservation.new('Girlie', Date.new(2018, 1, 17), Date.new(2018, 1, 31))
   end # end before
 
   describe 'initialize' do
@@ -10,22 +14,16 @@ describe 'Reservation' do
       @puppy_expo.guest_name.must_equal 'Finn'
       @puppy_expo.dates.check_in.to_s.must_equal '2017-10-13'
       @puppy_expo.dates.check_out.to_s.must_equal '2017-10-31'
-    end # end testr
+    end # end test
   end # end #initialize
-
-  describe 'reserve_dates' do
-    it 'returns the number of days to reserve' do
-      @puppy_expo.reserve_dates.must_equal 18
-    end # end test
-
-    it 'returns the array of dates needed to be reserved' do
-
-    end # end test
-  end # end #reserve_dates
-
   describe 'finalize_reservation' do
     it 'displays complete reservation info' do
       @puppy_expo.finalize_reservation.must_equal "Name: Finn\nCheck In: 2017-10-13\nCheck Out: 2017-10-31\nRoom Number: 1\nTotal Cost: $3600"
     end # end test
+
+    it 'displays different info for each new reservation w/o overwrite' do
+      @puppy_expo.finalize_reservation.must_equal "Name: Finn\nCheck In: 2017-10-13\nCheck Out: 2017-10-31\nRoom Number: 1\nTotal Cost: $3600"
+      @kitten_expo.finalize_reservation.must_equal "Name: Girlie\nCheck In: 2018-01-17\nCheck Out: 2018-01-31\nRoom Number: 2\nTotal Cost: $2800"
+    end
   end # end #final_reservation
 end # end of all Reservation tests
