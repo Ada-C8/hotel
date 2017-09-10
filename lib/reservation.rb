efee7b2221cbf271e96273bf2d922c37f82697c3
Sara Frandsen <sara.frandsen@gmail.com>
2017-09-10 00:09:03 -0700

    create hash of booking info

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -5,7 +5,7 @@ require_relative 'room'
 module Hotel
   class Reservation
     COST = 200 # per night
-    attr_reader :guest_name, :dates, :room, :reservation :list
+    attr_reader :guest_name, :dates, :room, :reservation, :cost, :list
 
     def initialize(guest_name, check_in, check_out)
       @guest_name = guest_name
@@ -13,23 +13,28 @@ module Hotel
       @check_out = check_out
       @dates = Hotel::DateRange.new(check_in, check_out)
       @room = Hotel::Room.new
-      @reservation = reservation
-      @list = list
+      @cost = (COST * @dates.length_of_stay).to_i
+      @list = {
+        :guest_name => @guest_name,
+        :check_in => @check_in.to_s,
+        :check_out => @check_out.to_s,
+        :cost => @cost
+      }
 
       #reservation: [name: date_range, room, cost]
       #sort_by{|k,v|v}
     end # end initialize
 
-    def finalize_reservation
-        booking = "Name: #{@guest_name}\nCheck In: #{@check_in}\nCheck Out: #{@check_out}\nRoom Number: #{@room.assign_room}\nTotal Cost: $#{(COST * @dates.length_of_stay).to_i}"
+    def self.list_all
+    end #end #list_all
 
-        return booking
-    end # end #finalize_reservation
-
-    def list_by_date
-      @list = {
-
-      }
+    def self.list_by_date
     end # end #list_by_date
+
+    def display_reservation
+      booking = "Name: #{@guest_name}\nCheck In: #{@check_in}\nCheck Out: #{@check_out}\nRoom Number: #{@room.assign_room}\nTotal Cost: $#{@cost}"
+
+      return booking
+    end # end #display_reservation
   end # end of Reservation
 end # end of Hotel module
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -10,20 +10,28 @@ describe 'Reservation' do
   end # end before
 
   describe 'initialize' do
-    it 'initializes with guest name, date range from DateRange, and room number' do
+    it 'initializes with guest name, date range from DateRange, and room number from Room' do
       @puppy_expo.guest_name.must_equal 'Finn'
       @puppy_expo.dates.check_in.to_s.must_equal '2017-10-13'
       @puppy_expo.dates.check_out.to_s.must_equal '2017-10-31'
     end # end test
+
+    it 'creates a hash containing all information, including cost' do
+      @puppy_expo.list[:guest_name].must_equal 'Finn'
+      @puppy_expo.list[:check_in].must_equal '2017-10-13'
+      @puppy_expo.list[:check_out].must_equal '2017-10-31'
+      @puppy_expo.list[:cost].must_equal 3600
+    end
   end # end #initialize
-  describe 'finalize_reservation' do
+
+  describe 'display_reservation' do
     it 'displays complete reservation info' do
-      @puppy_expo.finalize_reservation.must_equal "Name: Finn\nCheck In: 2017-10-13\nCheck Out: 2017-10-31\nRoom Number: 1\nTotal Cost: $3600"
+      @puppy_expo.display_reservation.must_equal "Name: Finn\nCheck In: 2017-10-13\nCheck Out: 2017-10-31\nRoom Number: 1\nTotal Cost: $3600"
     end # end test
 
     it 'displays different info for each new reservation w/o overwrite' do
-      @puppy_expo.finalize_reservation.must_equal "Name: Finn\nCheck In: 2017-10-13\nCheck Out: 2017-10-31\nRoom Number: 1\nTotal Cost: $3600"
-      @kitten_expo.finalize_reservation.must_equal "Name: Girlie\nCheck In: 2018-01-17\nCheck Out: 2018-01-31\nRoom Number: 2\nTotal Cost: $2800"
+      @puppy_expo.display_reservation.must_equal "Name: Finn\nCheck In: 2017-10-13\nCheck Out: 2017-10-31\nRoom Number: 1\nTotal Cost: $3600"
+      @kitten_expo.display_reservation.must_equal "Name: Girlie\nCheck In: 2018-01-17\nCheck Out: 2018-01-31\nRoom Number: 2\nTotal Cost: $2800"
     end
-  end # end #final_reservation
+  end # end #display_reservation
 end # end of all Reservation tests
