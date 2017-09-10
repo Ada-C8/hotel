dce178ccbdcdb1a70490de7ecc84dae3a1f3c815
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-09-10 15:25:07 -0700

    implement block discount in #get_total

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,22 +1,25 @@
+require 'pry'
+
 module Hotel
   class Reservation
     require_relative 'date_range'
-    attr_reader :total_cost, :dates, :checkin, :checkout, :id, :hotel, :room
+    attr_reader :total_cost, :dates, :checkin, :checkout, :id, :hotel, :room, :block
 
     def initialize(room, checkin, checkout, hotel, block = false)
       @room = room
       @checkin = DateRange.validate(checkin)
       @checkout = DateRange.validate(checkout)
       @dates = DateRange.range_to(@checkin, @checkout)
-      @hotel = hotel
-      # @id = create_id
-      @block = block
+      @block = block ? hotel.block(block) : false
       get_total
     end
 
     def get_total
+      # puts "block is #{@block}"
       num_nights = @dates.length
       @total_cost = @room.cost * num_nights
+      @total_cost *= @block.discount_rate if @block
+      return @total_cost
     end
 
     def includes_dates?(checkfirst, checklast)
