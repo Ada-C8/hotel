c679ceb5f89406533c188b5d4d5f9a5632b3aff0
hellomarisamorris <hellomarisamorris@gmail.com>
2017-09-10 11:02:58 -0700

    Wave 3: block class testst passing

--- lib/block.rb
+++ lib/block.rb
@@ -1,39 +1,14 @@
 # block
 
 module HotelManagment
-  class block
+  class Block
     attr_reader :amount_of_rooms
-    
+
     BLOCKRATE = 100
 
     def initialize(amount_of_rooms)
       @amount_of_rooms = amount_of_rooms
     end
-
+    
   end #class end
 end #module end
-
-
-
-
-
-
-
-
-
-
-
-# require 'awesome_print'
-#
-# module HotelManagment
-#   class Room
-#     attr_reader :room_number
-#
-#     RATE = 200
-#
-#     def initialize(room_number)
-#       @room_number = room_number
-#     end
-#
-#   end #class end
-# end #module end
--- specs/block_spec.rb
+++ specs/block_spec.rb
@@ -1,1 +1,33 @@
 require_relative 'spec_helper'
+
+
+describe "Block Class" do
+
+
+  it "Can instantiate a block" do
+
+    new_block = HotelManagment::Block.new(5)
+    new_block.must_be_instance_of HotelManagment::Block
+
+  end
+
+  it "amount_of_rooms should be an integer" do
+    new_block = HotelManagment::Block.new(5)
+    # new_room.must_be_instance_of HotelManagment::Room
+    new_block.amount_of_rooms.must_be_instance_of Integer
+  end
+
+  it "Amount_of_rooms should be equal to 5" do
+    new_block = HotelManagment::Block.new(5)
+    new_block.amount_of_rooms.must_equal 5
+  end
+
+  it "RATE should be equal to 100" do
+    HotelManagment::Block::BLOCKRATE.must_equal 100
+  end
+
+  it "Rate should be an instance of a Constant" do
+    HotelManagment::Block::BLOCKRATE.must_equal 100
+  end
+
+end
