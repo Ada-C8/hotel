e2ccb9623a45dec02867cc64a4086bcd6dd939fd
Shaunna <skwiens@gmail.com>
2017-09-09 19:28:55 -0700

    view available rooms in block cli added

--- lib/cli.rb
+++ lib/cli.rb
@@ -44,38 +44,44 @@ module Hotel
     end
 
     def view_available_rooms
+      beginning_date = get_date_for(
+      "begin")
+
+      end_date = get_date_for("end")
+
+      available_rooms = hotel.get_available_rooms(beginning_date, end_date)
+
+      show_table_of_rooms(available_rooms)
     end
 
+
+    ####### COME BACK TO THIS TO SEE IF IT WORKS ##########
     def view_available_rooms_in_block
+      block = get_block
+
+      puts "Rooms still available for block ID:#{block.id} :"
+
+      rooms_available_in_block = hotel.get_available_rooms_from_block(bblock)
+
+      show_table_of_rooms(rooms_available_in_block)
 
     end
-
-    #############!##!!###!!!! DOESN'T WORK!!!!!!!!!!
 
     def change_room_cost
-      room_number = get_room_number
+      room = get_room
       new_cost = get_value
 
+      room.cost_per_night = new_cost
 
-      hotel.rooms.each do |room|
-        if room.number == room_number
-          room.cost_per_night = new_cost
-        end
-      end
+      puts "\nRoom #{room.number} has been changed to $#{room.cost_per_night} per night."
 
       view_rooms
     end
 
     def view_rooms
-      puts "The hotel has the following rooms: "
-      table = Terminal::Table.new do |t|
-        t << ["Room", "Cost per night"]
-        hotel.rooms.each do |room|
-          t << :separator
-          t << [room.number, room.cost_per_night]
-        end
-      end
-      puts table
+      puts "\nHere is a current list of the rooms and their prices: "
+
+      show_table_of_rooms(hotel.rooms)
     end
 
     def display_user_menu
@@ -89,6 +95,12 @@ module Hotel
       G. View all rooms in the hotel\n"
     end
 
+    def get_date_for(action)
+      puts "What date would you like to #{action}?"
+
+
+    end
+
     def get_menu_option
       choice = nil
       until choice =~ (/^[A-Ga-g]$/)
@@ -98,6 +110,21 @@ module Hotel
       return choice
     end
 
+    def get_block
+      while true
+        print "Block ID: "
+        block_id = gets.chomp
+
+        hotel.blocks.each do |block|
+          if block.id = block_id
+            return block
+          end
+        end
+        puts "I'm sorry, that ID doesn't exit."
+      end
+    end
+
+    ########## WRITE TESTS ###########
     def get_value
       print "\n\tNew Cost: "
       value = gets.chomp
@@ -109,17 +136,34 @@ module Hotel
       return value.to_i
     end
 
-    def get_room_number
+    ########## WRITE TESTS ##############
+    def get_room
       print "\n\tRoom Number: "
       room_number = gets.chomp
-      until room_number =~ (/^([1-9]|1[0123456789]|20)$/)
+      until room_number =~ (/^([1-9]|1[0-9]|20)$/)
         puts "Invalid Room Number."
         print "Room Number: "
         room_number = gets.chomp
       end
-      return room_number.to_i
+
+      hotel.rooms.each do |room|
+        if room.number == room_number.to_i
+          return room
+        end
+      end
+      raise StandardError.new "No room found"
     end
 
+    def show_table_of_rooms(rooms_arry)
+      table = Terminal::Table.new do |t|
+        t << ["Room", "Cost per night"]
+        rooms.each do |room|
+          t << :separator
+          t << [room.number, room.cost_per_night]
+        end
+      end
+      puts table
+    end
   end
 end # end of hotel module
 #
