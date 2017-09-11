7f5d2264e006c131e7447f46e402e054307cd16c
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-09-11 09:30:52 -0700

    Updated testing dates to avoid error from dates in past.

--- specs/reservations_spec.rb
+++ specs/reservations_spec.rb
@@ -12,22 +12,22 @@ describe "reservations class" do
     end
 
     it "allows you to book a room beginning on the ending date of the previous resveration" do
-      reservation = Reservation.new(5, 2017, 9, 15, 2017, 9, 16)
+      reservation = Reservation.new(5, 2017, 10, 15, 2017, 10, 16)
       reservation.must_be_instance_of Reservation
-      next_reservation = Reservation.new(5, 2017, 9, 16, 2017, 9, 17)
+      next_reservation = Reservation.new(5, 2017, 10, 16, 2017, 10, 17)
       next_reservation.must_be_instance_of Reservation
     end
 
     it "changes the status of a room to booked and does not allow another reservation to overlap" do
-      Reservation.new(1, 2017, 9, 18, 2017, 9, 19)
-      proc { Reservation.new(1, 2017, 9, 18, 2017, 9, 19)}.must_raise ArgumentError
+      Reservation.new(1, 2017, 9, 28, 2017, 9, 29)
+      proc { Reservation.new(1, 2017, 9, 28, 2017, 9, 29)}.must_raise ArgumentError
     end
 
     it "can return the total cost of a reservation" do
-      reservation = Reservation.new(1, 2017, 9, 18, 2017, 9, 19)
+      reservation = Reservation.new(1, 2017, 9, 28, 2017, 9, 29)
       reservation.total_cost.must_be_instance_of Integer
       reservation.total_cost.must_equal 200
-      another_reservation = Reservation.new(2, 2017, 9, 18, 2017, 9, 21)
+      another_reservation = Reservation.new(2, 2017, 9, 28, 2017, 10, 1)
       another_reservation.total_cost.must_be_instance_of Integer
       another_reservation.total_cost.must_equal 600
     end
