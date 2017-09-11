da1e6f4bd6c46232149202c784119e430bafca83
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-10 22:55:04 -0700

    Cleaned up Block class

--- lib/block.rb
+++ lib/block.rb
@@ -9,7 +9,6 @@ module Hotel
     def initialize(id, rooms, date_range, block_info)
       super(id, rooms, date_range)
       @block_info = block_info
-      # @reserved_rooms = []
     end
 
     def rooms_available_block
@@ -21,25 +20,5 @@ module Hotel
       end
       return rooms_available
     end
-
-    # def book_room_in_block(num_rooms)
-    #   if num_rooms > 5 || num_rooms > @rooms.length
-    #
-    #   rooms_available_block.length
-    #     raise InvalidRoomQuantity.new("Unfortunately the hotel does not have enough rooms left in the block to handle your request of #{num_rooms} rooms.")
-    #   end
-    #   #
-    #   # rooms_arr = []
-    #   # num_rooms.times do
-    #   #   copy = self.rooms.shift(1)
-    #   #   copy1 = copy
-    #   #   self.reserved_rooms << copy1
-    #   #   rooms_arr << copy1
-    #   # end
-    #   #
-    #   # booking = Hotel::Booking.new(0,rooms_arr, @date_range, [@block_name, @discount_room_price])
-    #   #
-    #   # return booking
-    # end
   end
 end
