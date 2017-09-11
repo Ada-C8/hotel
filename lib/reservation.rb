b37015de904a2cad7021164a4e6e397e6d013c8e
nkiru onwuneme <generalhrh@gmail.com>
2017-09-11 05:20:54 -0700

    Edited initialize method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,18 +1,29 @@
 module Hotel
   class Reservation
-    attr_reader :dates
+    attr_reader :name, :reserved_rooms, :date_range
 
-    ROOMCOST = 200
-
-    def initialize(checkin_date, checkout_date)
-      @dates = DateRange.new(checkin_date, checkout_date)
-      @num_nights = checkout_date - checkin_date
+    def initialize(name, reserved_rooms, date_range)
+      @name = name
+      @reserved_rooms = reserved_rooms
+      @date_range = date_range
+      # puts @reserved_rooms # test if list of all rooms is printing
+      # puts @name
+      # puts @date_range
     end
 
-    def price_night
-      total_cost = (@num_nights-1) * ROOMCOST
+    def total_cost
+      num_nights = checkout_date - checkin_date
+      total_cost  = num_nights * room_rate
       return total_cost
     end
+
+
+
+
+
+
+
+
   end
 
 end
