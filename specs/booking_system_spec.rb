c7837c073bc53c01f2cc1af701396e8bd7eccc27
nkiru onwuneme <generalhrh@gmail.com>
2017-09-11 06:45:26 -0700

    Added additional tests for initialize method

--- specs/booking_system_spec.rb
+++ specs/booking_system_spec.rb
@@ -8,8 +8,28 @@ describe "BookingSystem" do
       new_reservation.must_be_instance_of Hotel::BookingSystem
     end
 
+    it "creates 20 room objects" do
+      new_reservation =
+      Hotel::BookingSystem.new
+      new_reservation.collection_of_rooms.length.must_equal 20
+    end
+
+    it "checks that collection of rooms is an array" do
+      new_reservation =
+      Hotel::BookingSystem.new
+      new_reservation.collection_of_rooms.must_be_kind_of Array
+    end
   end
-end
+
+  # describe "create rooms" do
+  #   it "returns the room number" do
+  #     new_reservation =
+  #     Hotel::BookingSystem.new
+  # #     new_reservation.collection_of_rooms[2].must_equal 3
+  # #   end
+  # end
+
+end #end of describe block
 
 
 
