480123a7fc8ff3b1177f3a8a2b969f1b5b909b51
nkiru onwuneme <generalhrh@gmail.com>
2017-09-11 06:36:22 -0700

    Added initialize method

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -1,13 +1,29 @@
+require_relative 'room'
+require_relative 'reservation'
 require_relative 'date_range'
+require 'date'
+
 module Hotel
   class BookingSystem
-      attr_reader :rooms, :reservations_rooms_hash
+    attr_reader :collection_of_rooms, :collection_of_reservations
 
-#Access list of all rooms in the hotel.
-      def initialize
-        @rooms = Hotel::Room.all # {ID : RoomObject}
-        @collection_of_reservation = {} # key: reservation, value: hash
+    #Access list of all rooms in the hotel.
+    def initialize
+      @collection_of_rooms = []
+      build_rooms
+      # 20.times do |i|
+      #   room = Hotel::Room.new(i + 1, 200)
+      #   @collection_of_rooms << room
+      # end
+      @collection_of_reservations = []
+    end
+
+    def build_rooms
+      20.times do |i|
+        room = Hotel::Room.new(i + 1, 200)
+        @collection_of_rooms << room
       end
+    end
 
-  end
-end
+  end #end of class
+end #end of module
