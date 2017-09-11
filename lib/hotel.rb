67bbbfff22fb2583c3c1c93a4cf2a9b4f701b672
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-11 04:04:16 -0700

    require custom error classes

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -3,3 +3,6 @@ require_relative "room.rb"
 require_relative "admin.rb"
 require_relative "reservation.rb"
 require_relative "block_reservation.rb"
+
+#Errors
+Dir[File.dirname(__FILE__) + '/errors/*.rb'].each {|file| require file }
