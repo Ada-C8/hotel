a505e0859a38401118b940ae856b56da1a402366
nkiru onwuneme <generalhrh@gmail.com>
2017-09-11 08:48:50 -0700

    Edited tests

--- specs/booking_system_spec.rb
+++ specs/booking_system_spec.rb
@@ -7,33 +7,39 @@ describe "BookingSystem" do
       Hotel::BookingSystem.new
       new_reservation.must_be_instance_of Hotel::BookingSystem
     end
-
-    it "creates 20 room objects" do
-      new_reservation =
-      Hotel::BookingSystem.new
-      new_reservation.collection_of_rooms.length.must_equal 20
-    end
-
-    it "checks that collection of rooms is an array" do
-      new_reservation =
-      Hotel::BookingSystem.new
-      new_reservation.collection_of_rooms.must_be_kind_of Array
-    end
   end
 
-  # describe "create rooms" do
-  #   it "returns the room number" do
-  #     new_reservation =
-  #     Hotel::BookingSystem.new
-  # #     new_reservation.collection_of_rooms[2].must_equal 3
-  # #   end
-  # end
+  describe "room availability" do
+    it "checks the room availabilty and if the date range overlaps" do
+      new_reservation = Hotel::BookingSystem.new
+      checkin_date = Date.new(2001,02,2)
+      checkout_date = Date.new(2001,02,10)
+      dates = Hotel::DateRange.new(checkin_date, checkout_date)
+      puts new_reservation.room_available(1,dates)
+
+    end
+  end
+#
+#     it "creates 20 room objects" do
+#       new_reservation =
+#       Hotel::BookingSystem.new
+#       new_reservation.collection_of_rooms.length.must_equal 20
+#     end
+#
+#     it "checks that collection of rooms is an array" do
+#       new_reservation =
+#       Hotel::BookingSystem.new
+#       new_reservation.collection_of_rooms.must_be_kind_of Array
+#     end
+#   end
+#
+#   # describe "create rooms" do
+#   #   it "returns the room number" do
+#   #     new_reservation =
+#   #     Hotel::BookingSystem.new
+#   # #     new_reservation.collection_of_rooms[2].must_equal 3
+#   # #   end
+#   # end
+#
 
 end #end of describe block
-
-
-
-
-
-#
-# end #End of describe block
