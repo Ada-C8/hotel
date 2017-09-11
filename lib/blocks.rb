84771c5185d925aa0a270014014b6fa23504ce41
Tanisha <tanishahampden@gmail.com>
2017-09-11 07:48:26 -0700

    update create_block method and tests

--- lib/blocks.rb
+++ lib/blocks.rb
@@ -1,10 +1,24 @@
 module Hotel
   class Blocks
-    attr_reader :date_range, :rooms, :discounted_rate
-    def initialize(check_in,check_out)
-      @date_range = Hotel::DateRange.new(check_in,check_out)
-      @rooms = []
-      @discounted_rate = 0.2
+    attr_reader :date_range, :block_rooms, :discount, :hotel
+    def initialize
+      @hotel = Hotel::HotelClass.new
+      @block_rooms = []
+      @discount = 0.2
+    end
+
+    # currently broken/incomplete
+    def create_block(room,check_in,check_out)
+      # check what rooms are available
+      available_for_block = hotel.view_available_rooms(check_in,check_out)
+      # push 5 available room onto block_rooms
+      ap "printing available:#{available_for_block}"
+      available_for_block.each do |n|
+        if block_rooms.length < 5
+          block_rooms << n
+        end
+      end
+      ap "printing block_rooms :#{block_rooms}"
     end
 
   end
