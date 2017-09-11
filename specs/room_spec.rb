5d95ca191b555381f9760ff267d73284e5e8842d
nkiru onwuneme <generalhrh@gmail.com>
2017-09-11 05:07:42 -0700

    Added additional tests for initialize method

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -7,10 +7,15 @@ describe "Room" do
       room_rate = 200
       room = Hotel::Room.new(room_num, room_rate)
       room.must_be_instance_of Hotel::Room
+
+      room.room_num.must_equal room_num
+      room.must_respond_to :room_num
+
+      room.room_rate.must_equal 200
+      room.room_rate.must_be_kind_of Integer
     end
 
     it "returns rooms 1, 5, 10, 13 and 20" do
-    
       room_num = 1
       room_rate = 200
       room = Hotel::Room.new(room_num, room_rate)
@@ -31,36 +36,6 @@ describe "Room" do
       room_num = 20
       room = Hotel::Room.new(room_num, 200)
       room.room_num.must_equal 20
-
     end
   end
 end
-
-# # probably can use let to trim code
-#   describe "self.all" do
-#     it "returns an array of all rooms" do
-#       rooms = Hotel::Room.all
-#       rooms.must_be_kind_of Array
-#     end
-#
-#     it "checks the number of rooms is correct" do
-#       rooms = Hotel::Room.all
-#       rooms.length.must_equal 20
-#     end
-#
-#     # it "checks the first, tenth, and twentienth rooms can be retrieved" do
-#     #   rooms = Hotel::Room.all
-#     #   rooms.each do |room|
-#     #     room[8].must_equal 7
-#     #   end
-#     #   # puts rooms[0]
-#     #   # rooms[0].must_equal Hotel::Room.all
-#     # end
-#
-#     it "checks everything in the array is a room" do
-#       rooms = Hotel::Room.all
-#       rooms.each do |room|
-#         room.must_be_instance_of Hotel::Room
-#       end
-#     end
-#   end
