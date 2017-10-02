31f6a4c295c03c805972b1930fa383ec6287bebc
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-01 18:13:27 -0700

    date_range_spec initialize tests pass

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -1,25 +1,51 @@
 require_relative 'spec_helper'
 
 describe 'DateRange' do
-  before do
-    @puppy_dates = Hotel::DateRange.new(Date.new(2017, 10, 13), Date.new(2017, 10, 31))
-  end # end before
 
   describe 'initialize' do
-    it 'takes a check in date and a check out date' do
-      @puppy_dates.check_in.to_s.must_equal '2017-10-13'
-      @puppy_dates.check_out.to_s.must_equal '2017-10-31'
-    end # end test
+    it 'passes in two Date objects as check in and check out arguments' do
+      check_in = Date.new(2017, 10, 13)
+      check_out = Date.new(2017, 10, 31)
+      honeymoon = Hotel::DateRange.new(check_in, check_out)
 
-    it 'passes check_in and check_out in as Date objects' do
-      @puppy_dates.check_in.class.must_equal Date
-      @puppy_dates.check_out.class.must_equal Date
-    end # end test
-  end # end #initialize
+      honeymoon.check_in.must_be_kind_of Date
+      honeymoon.check_out.must_be_kind_of Date
+    end
 
-  describe 'length_of_stay' do
-    it 'finds the number of days needed for the reservation based on given dates' do
-      @puppy_dates.length_of_stay.must_equal 18
-    end # end test
-  end # end #length_of_stay
+    it 'cannot have a negative number of nights' do
+      check_out = Date.new(2017, 10, 13)
+      check_in = Date.new(2017, 10, 31)
+
+      proc { Hotel::DateRange.new(check_in, check_out) }.must_raise Hotel::DateRange::InvalidDateRange
+    end
+
+    it 'cannot book zero nights' do
+      check_out = Date.new(2017, 10, 13)
+      check_in = Date.new(2017, 10, 13)
+
+      proc { Hotel::DateRange.new(check_in, check_out) }.must_raise Hotel::DateRange::InvalidDateRange
+    end
+  end
+
+  # before do
+  #   @puppy_dates = Hotel::DateRange.new(Date.new(2017, 10, 13), Date.new(2017, 10, 31))
+  # end # end before
+  #
+  # describe 'initialize' do
+  #   it 'takes a check in date and a check out date' do
+  #     @puppy_dates.check_in.to_s.must_equal '2017-10-13'
+  #     @puppy_dates.check_out.to_s.must_equal '2017-10-31'
+  #   end # end test
+  #
+  #   it 'passes check_in and check_out in as Date objects' do
+  #     @puppy_dates.check_in.class.must_equal Date
+  #     @puppy_dates.check_out.class.must_equal Date
+  #   end # end test
+  # end # end #initialize
+  #
+  # describe 'length_of_stay' do
+  #   it 'finds the number of days needed for the reservation based on given dates' do
+  #     @puppy_dates.length_of_stay.must_equal 18
+  #   end # end test
+  # end # end #length_of_stay
 end # end of all DateRange tests
