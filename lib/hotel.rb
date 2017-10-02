364de8fafa1472dfeff5ac26fe3d96913cf1e327
Tanisha <tanishahampden@gmail.com>
2017-10-01 23:04:25 -0700

    modify view_avaiable_rooms and reserve_rooms methods

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -2,26 +2,48 @@ require_relative 'reservation'
 
 module Hotel
   class Hotel
-    attr_reader :rooms, :reservations, :date_range, :available_room_nums, :reserved, :reserved_room_nums
+    attr_reader :rooms, :reservations, :reserved
     def initialize
       # access the list of all of the rooms in the hotel
       @rooms = (1..20).to_a
       @reservations = []
-      @available_room_nums = []
       @reserved = []
-      @reserved_room_nums = []
+    end
+
+    def view_available_rooms(check_in, check_out)
+      # view a list of rooms that are not reserved for a given date range
+      # loop thru reservations.date_range to see if it include the date_range passed in, if not return room_ids
+
+      available_rooms = @rooms
+      reserved_rooms = []
+
+      reservations.each do |reservation|
+        if reservation.date_range.overlap?(check_in, check_out)
+          reserved << reservation
+        end
+      end
+
+      reserved.each do |reserved_room|
+        reserved_rooms << reserved_room.room_id
+      end
+
+      available_rooms -= reserved_rooms
+      return available_rooms
     end
 
     def reserve_room(room_id, check_in, check_out)
-      # reserve an available room for a given date range
-      new_reservation = Reservation.new(room_id, check_in, check_out)
-      # if room_id is not reserved
-      if !(reserved_room_nums.include?(room_id))
-        reservations << new_reservation
-      else
-        # currently does not raise Error
-        raise ArgumentError.new("That date range is unavailable.")
+      unless @rooms.include?(room_id)
+        raise ArgumentError.new("That room does not exist")
       end
+
+      unless view_available_rooms(check_in, check_out).include?(room_id)
+        raise ArgumentError.new("Room #{room_id} is not available for those dates")
+      end
+
+      new_reservation = Reservation.new(room_id, check_in, check_out)
+      reservations << new_reservation
+
+      return new_reservation
     end
 
     def access_reservations(date)
@@ -29,7 +51,6 @@ module Hotel
       # loop over @reservations.date_range
       # if date_range includes passed in date
       # return reservations
-
 
       reservations.each do |reservation|
         # I am not requiring a new instance of DateRange class anywhere in this document
@@ -42,25 +63,5 @@ module Hotel
       return reservations
     end
 
-    def view_available_rooms(check_in, check_out)
-      # view a list of rooms that are not reserved for a given date range
-      # loop thru reservations.date_range to see if it include the date_range passed in, if not return room_ids
-      reservations.each do |reservation|
-        if reservation.date_range.overlap?(check_in, check_out)
-          reserved << reservation
-        end
-      end
-      reserved.each do |reserved_room|
-        if !(reserved.include?(reserved_room.room_id))
-          reserved_room_nums << reserved_room.room_id
-        end
-      end
-      rooms.each do |room|
-        if !(reserved_room_nums.include?(room))
-          available_room_nums << room
-        end
-      end
-      return available_room_nums
-    end
   end
 end
