fa1cddfc3f739493acab9ddb118832a3726a7e7f
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-09 19:38:18 -0700

    Added @bookings array to Room

--- lib/room.rb
+++ lib/room.rb
@@ -2,9 +2,10 @@ require 'date'
 
 module Hotel
   class Room
-    attr_reader :price
+    attr_reader :price, :bookings
     def initialize(price = 200)
-      @price = price.to_f
+      @price = price
+      @bookings = Array.new
     end
     # def booked?(arrival, departure)
     #   arrival = Date.strptime('03-02-2001', '%m-%d-%Y')
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,13 +1,33 @@
 require_relative 'spec_helper.rb'
 
-describe "hotel" do
+
+describe "Room" do
   before do
     @room = Hotel::Room.new
+    @room2 = Hotel::Room.new(400)
   end
-  it "has a hotel class" do
-    Hotel::Room.class.must_be_kind_of Class
+
+  describe "Room class" do
+    it "has a room class" do
+      Hotel::Room.class.must_be_kind_of Class
+    end
   end
-  it "return @price == 200 is no parameter is passed" do
-    @room.price.must_equal 200
+
+  describe "@price" do
+
+    it "return @price == 200 if no parameter is passed" do
+      @room.price.must_equal 200
+    end
+
+    it "returns the price given" do
+      @room2.price.must_equal 400
+    end
   end
+
+  describe "@bookings" do
+    it "has a @booking variable" do
+      @room.must_respond_to :bookings
+    end
+  end
+
 end
